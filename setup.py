33ef81f5320aad284a8f0610f3e5d33130b0f3de
TarikExner <Tarik.Exner@gmail.com>
2023-05-04 16:35:57 +0200

    repo_init

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='FACSPy',
+version='0.1',
+description='An example package',
+url='#',
+author='max',
+install_requires=[],
+author_email='',
+packages=setuptools.find_packages(),
+zip_safe=False)
