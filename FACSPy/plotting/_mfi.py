4e80638b97fe16a88dc8f628c0f0e5c4d88a28a6
TarikExner <Tarik.Exner@gmail.com>
2023-06-17 22:18:06 +0200

    small changes

--- FACSPy/plotting/_mfi.py
+++ FACSPy/plotting/_mfi.py
@@ -1,9 +1,10 @@
 from anndata import AnnData
 import pandas as pd
+import numpy as np
 from typing import Union, Literal
 
 from matplotlib.figure import Figure
-from matplotlib.axes import Axes
+from matplotlib.axis import Axis
 
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -12,52 +13,164 @@ from ..utils import find_gate_path_of_gate
 
 from ..exceptions.exceptions import AnalysisNotPerformedError
 
-from .utils import create_boxplot
+from .utils import create_boxplot, append_metadata, turn_off_missing_plots, prep_uns_dataframe
 ### mfi plot
 
-def append_metadata(adata: AnnData,
-                    mfi_data: pd.DataFrame) -> pd.DataFrame:
-    mfi_data = mfi_data.T
-    mfi_data.index = mfi_data.index.set_names(["sample_ID", "gate_path"])
-    mfi_data["sample_ID"] = mfi_data["sample_ID"].astype("int64")
-    metadata = adata.uns["metadata"].to_df()
-
-    return pd.merge(mfi_data, metadata, on = "sample_ID")
 
 
-
+def fop(adata: AnnData,
+        markers: Union[str, list[str]],
+        groupby: Union[str, list[str]],
+        order: list[str] = None,
+        gate: str = None,
+        overview: bool = False):
+    
+    try:
+        fop_data = adata.uns["fop"]
+        fop_data = prep_uns_dataframe(adata, fop_data)
+    except KeyError as e:
+        raise AnalysisNotPerformedError("fop") from e
+    
+    mfi_fop_baseplot(adata = adata,
+                     dataframe = fop_data,
+                     markers = markers,
+                     groupby = groupby,
+                     gate = gate,
+                     assay = "fop",
+                     overview = overview,
+                     order = order)
 
 def mfi(adata: AnnData,
         markers: Union[str, list[str]],
-        groupby = Union[str, list[str]],
-        gate: str = None):
+        order: list[str] = None,
+        groupby: Union[str, list[str]] = None,
+        gate: str = None,
+        overview: bool = False):
 
     try:
         mfi_data = adata.uns["mfi"]
-    except KeyError:
-        raise AnalysisNotPerformedError("mfi")
+        mfi_data = prep_uns_dataframe(adata, mfi_data)
+    except KeyError as e:
+        raise AnalysisNotPerformedError("mfi") from e
 
+    mfi_fop_baseplot(adata = adata,
+                     dataframe = mfi_data,
+                     markers = markers,
+                     groupby = groupby,
+                     gate = gate,
+                     assay = "mfi",
+                     overview = overview,
+                     order = order)
+
+
+from itertools import combinations
+def create_comparisons(data: pd.DataFrame,
+                       groupby: str) -> list[tuple[str, str]]:
+    return list(combinations(data[groupby].unique(), 2))
+
+from statannotations.Annotator import Annotator
+def add_statistic(ax: Axis,
+                  test: str,
+                  dataframe: pd.DataFrame,
+                  groupby: str,
+                  marker: str,
+                  plot_params) -> Axis:
+    
+    pairs = create_comparisons(dataframe, groupby)
+
+    annotator = Annotator(ax,
+                          pairs,
+                          **plot_params,
+                          verbose = False)
+    annotator.configure(test = test, text_format = "star", loc = "inside")
+    annotator.apply_and_annotate()
+
+    return ax
+    
+    
+
+def mfi_fop_baseplot(adata: AnnData,
+                     dataframe: pd.DataFrame,
+                     markers: Union[str, list[str]],
+                     groupby: Union[str, list[str]],
+                     assay: Literal["mfi", "fop"],
+                     order: list[str] = None,
+                     gate: str = None,
+                     overview: bool = False):
+    
     if gate is None:
         raise TypeError("A Gate has to be provided")
+    if overview:
+        if markers:
+            print("warning... markers argument is ignored when using overview")
+        markers = adata.var_names.to_list()
     
-    if not isinstance(markers):
+    if not isinstance(markers, list):
         markers = [markers]
     if not isinstance(groupby, list):
         groupby = [groupby]
 
-    ncols = 1,
-    nrows = len(groupby)
-    figsize = (3, 3 * len(groupby)) 
-    
-    fig, ax = plt.subplots(ncols = 1, nrows = nrows, figsize = figsize)
-
-
-    full_gate_path = find_gate_path_of_gate(adata, gate)
-    gate_specific_mfis = mfi_data.loc[mfi_data["gate_path"] == full_gate_path, :]
     for grouping in groupby:
-        for marker in markers:
+        ncols = 4 if overview else 1
+        nrows = int(np.ceil(len(markers) / 4)) if overview else len(markers)
+        figsize = (12 if overview
+                   else 3,
+                   int(np.ceil(len(markers) / 4)) * 3 if overview
+                   else 5* len(markers)) 
+        fig, ax = plt.subplots(ncols = ncols, nrows = nrows, figsize = figsize)
+        if len(markers) > 1:
+            ax = ax.flatten()
+        for i, marker in enumerate(markers):
+            print(marker)
             plot_params = {
                 "x": "sample_ID" if grouping is None else grouping,
                 "y": marker,
-                    "data": gate_specific_mfis
+                "data": gate_specific_mfis,
+                "order": order
             }
+            if len(markers)>1:
+                ax[i] = create_boxplot(ax = ax[i],
+                                    grouping = grouping,
+                                    plot_params = plot_params)
+                ax[i] = label_plot(ax = ax[i],
+                                   marker = marker,
+                                   grouping = grouping,
+                                   assay = assay)
+                ax[i] = add_statistic(ax = ax[i],
+                                      test = "Kruskal",
+                                      dataframe = gate_specific_mfis,
+                                      groupby = "sample_ID" if grouping is None else grouping,
+                                      marker = marker,
+                                      plot_params = plot_params)
+            else:
+                ax = create_boxplot(ax = ax,
+                                    grouping = grouping,
+                                    plot_params = plot_params)
+                ax = label_plot(ax = ax,
+                                marker = marker,
+                                grouping = grouping,
+                                assay = assay)
+                ax = add_statistic(ax = ax,
+                                   test = "Kruskal",
+                                   dataframe = gate_specific_mfis,
+                                   groupby = "sample_ID" if grouping is None else grouping,
+                                   marker = marker,
+                                   plot_params = plot_params)
+        if len(markers) > 1:
+            ax = turn_off_missing_plots(ax)
+        ax = np.reshape(ax, (ncols, nrows))
+        
+        plt.tight_layout()
+        plt.show()
+
+
+
+def label_plot(ax: Axis,
+               marker: str,
+               grouping: str,
+               assay: Literal["mfi", "fop"]) -> Axis:
+    ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = "center")
+    ax.set_title(f"{marker} expression\nper {grouping}")
+    ax.set_ylabel("expression" if assay == "mfi" else "fraction positive")
+    ax.set_xlabel("")
+    return ax
--- FACSPy/plotting/utils.py
+++ FACSPy/plotting/utils.py
@@ -4,22 +4,46 @@ import pandas as pd
 
 import seaborn as sns
 
+from anndata import AnnData
 
 from matplotlib.axis import Axis
 
+def prep_uns_dataframe(adata: AnnData,
+                       data: pd.DataFrame) -> pd.DataFrame:
+    data = data.T
+    data.index = data.index.set_names(["sample_ID", "gate_path"])
+    data = data.reset_index()
+    data["sample_ID"] = data["sample_ID"].astype("int64")
+    return append_metadata(adata, data)
+
+
+def map_obs_to_cmap(data: pd.DataFrame,
+                    parameter_to_map: str,
+                    cmap: str = "Set1") -> dict[str, tuple[float, float, float]]:
+    obs = data[parameter_to_map].unique()
+    cmap = sns.color_palette(cmap, len(obs))
+    mapping = {obs_entry: cmap[i] for i, obs_entry in enumerate(obs)}
+    return data[parameter_to_map].map(mapping)
+
+def append_metadata(adata: AnnData,
+                    dataframe_to_merge: pd.DataFrame) -> pd.DataFrame:
+    metadata = adata.uns["metadata"].to_df()
+    return pd.merge(dataframe_to_merge, metadata, on = "sample_ID")
+
 def create_boxplot(ax: Axis,
-                grouping: str,
-                plot_params: dict) -> Axis:
+                   grouping: str,
+                   plot_params: dict) -> Axis:
     
     if grouping is None or grouping == "sample_ID":
-        sns.barplot(**plot_params)
+        sns.barplot(**plot_params,
+                    ax = ax)
     
     else:
         sns.stripplot(**plot_params,
-                        dodge = True,
-                        jitter = True,
-                        linewidth = 1,
-                        ax = ax)
+                      dodge = True,
+                      jitter = True,
+                      linewidth = 1,
+                      ax = ax)
         sns.boxplot(**plot_params,
                     boxprops = dict(facecolor = "white"),
                     whis = (0,100),
