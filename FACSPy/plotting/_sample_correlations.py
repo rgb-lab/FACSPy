33b7a07dcc293fe37ee07f58d1593139f92a8df2
TarikExner <Tarik.Exner@gmail.com>
2023-06-22 13:07:11 +0200

    added metaclustering

--- FACSPy/plotting/_sample_correlations.py
+++ FACSPy/plotting/_sample_correlations.py
@@ -14,27 +14,37 @@ from .utils import (prep_uns_dataframe,
                     turn_off_missing_plots,
                     scale_data,
                     select_gate_from_singleindex_dataframe,
-                    map_obs_to_cmap)
-from ..utils import find_gate_path_of_gate, reduction_names
+                    map_obs_to_cmap,
+                    calculate_metaclusters,
+                    map_metaclusters_to_sample_ID,
+                    merge_metaclusters_into_dataframe)
+from ..utils import find_gate_path_of_gate, reduction_names, subset_gate
 
 from ..exceptions.exceptions import AnalysisNotPerformedError
+
+from scipy.spatial import distance
+from scipy.cluster import hierarchy
 
 
 def marker_correlation(adata: AnnData,
                        gate: str,
                        scaling: Literal["MinMaxScaler", "RobustScaler"] = "MinMaxScaler",
-                       on: Literal["mfi", "fop", "gate_frequency"] = "mfi",
+                       on: Literal["mfi", "fop", "gate_frequency", "all_cells"] = "mfi",
                        corr_method: Literal["pearson", "spearman", "kendall"] = "pearson",
                        cmap: str = "inferno",
                        return_fig: bool = False) -> Optional[Figure]:
     
     try:
-        data = adata.uns[on]
-        data = prep_uns_dataframe(adata, data)
-        data = select_gate_from_singleindex_dataframe(data, find_gate_path_of_gate(adata, gate))
+        if on == "all_cells":
+            adata = subset_gate(adata, gate, as_view = True)
+            data = adata.to_df(layer = "transformed")
+            
+        else:
+            data = adata.uns[on]
+            data = prep_uns_dataframe(adata, data)
+            data = select_gate_from_singleindex_dataframe(data, find_gate_path_of_gate(adata, gate))
         fluo_columns = [col for col in data.columns if col in adata.var_names.to_list()]
         data[fluo_columns] = scale_data(data[fluo_columns], scaling)
-        
     except KeyError as e:
         raise AnalysisNotPerformedError(on) from e
 
@@ -65,6 +75,9 @@ def marker_correlation(adata: AnnData,
         return clustermap
     plt.show()
 
+
+
+
 def sample_correlation(adata: AnnData,
                        groupby: Optional[Union[str, list[str]]],
                        gate: str,
@@ -72,7 +85,10 @@ def sample_correlation(adata: AnnData,
                        on: Literal["mfi", "fop", "gate_frequency"] = "mfi",
                        corr_method: Literal["pearson", "spearman", "kendall"] = "pearson",
                        cmap: str = "inferno",
-                       return_fig: bool = False) -> Optional[Figure]:
+                       return_fig: bool = False,
+                       metaclusters: Optional[int] = None,
+                       label_metaclusters_in_dataset: bool = True,
+                       label_metaclusters_key: Optional[str] = "metacluster_sc") -> Optional[Figure]:
     
     try:
         data = adata.uns[on]
@@ -80,28 +96,60 @@ def sample_correlation(adata: AnnData,
         data = select_gate_from_singleindex_dataframe(data, find_gate_path_of_gate(adata, gate))
         fluo_columns = [col for col in data.columns if col in adata.var_names.to_list()]
         data[fluo_columns] = scale_data(data[fluo_columns], scaling)
-        
+
     except KeyError as e:
         raise AnalysisNotPerformedError(on) from e
-    
+
     if not isinstance(groupby, list):
         groupby = [groupby]
 
     #fig, ax = plt.subplots(ncols = 1, nrows = 1, figsize = (5,5))
     annotation_cmaps = ["Set1", "Set2", "tab10", "hls", "Paired",]
-    clustermap = sns.clustermap(data = data[fluo_columns].T.corr(method = corr_method),
-                                row_colors = [map_obs_to_cmap(data, group, annotation_cmaps[i]) for i, group in enumerate(groupby)],
-                                col_colors = [map_obs_to_cmap(data, group, annotation_cmaps[i]) for i, group in enumerate(groupby)],
-                                vmin = -1,
-                                vmax = 1,
-                                cmap = cmap,
-                                dendrogram_ratio = (.1, .1),
-                                annot_kws = {"size": 4},
-                                figsize = (5,3.8),
-                                cbar_kws = {"label": f"{corr_method} correlation",
-                                            "orientation": 'horizontal'},
-                                yticklabels = False,
-                                xticklabels = False)
+    sample_IDs = data["sample_ID"].to_list()
+
+    correlation_data = data[fluo_columns].T.corr(method = corr_method)
+
+    row_linkage = hierarchy.linkage(distance.pdist(correlation_data.to_numpy()), method='average')
+    col_linkage = hierarchy.linkage(distance.pdist(correlation_data.to_numpy().T), method='average')
+
+    if metaclusters is not None:
+        metaclusters = calculate_metaclusters(row_linkage, n_clusters = metaclusters)
+        metacluster_mapping = map_metaclusters_to_sample_ID(metaclusters, sample_IDs)
+        data = merge_metaclusters_into_dataframe(data, metacluster_mapping)
+        groupby += ["metacluster"]
+        if label_metaclusters_in_dataset:
+            if "metacluster" in adata.uns["metadata"].dataframe:
+                print("warninig... overwriting metaclusters")
+                adata.uns["metadata"].dataframe = adata.uns["metadata"].dataframe.drop(["metacluster"], axis = 1)
+            adata.uns["metadata"].dataframe = pd.merge(adata.uns["metadata"].dataframe, data[["sample_ID", "metacluster"]], on = "sample_ID")
+            if label_metaclusters_key is not None:
+                adata.uns["metadata"].dataframe[label_metaclusters_key] = adata.uns["metadata"].dataframe["metacluster"]
+                adata.uns["metadata"].dataframe = adata.uns["metadata"].dataframe.drop(["metacluster"], axis = 1)
+
+
+    clustermap = sns.clustermap(
+        data = correlation_data,
+        row_colors=[
+            map_obs_to_cmap(data, group, annotation_cmaps[i])
+            for i, group in enumerate(groupby)
+        ],
+        col_colors=[
+            map_obs_to_cmap(data, group, annotation_cmaps[i])
+            for i, group in enumerate(groupby)
+        ],
+        row_linkage = row_linkage,
+        col_linkage = col_linkage,
+        vmin = -1,
+        vmax = 1,
+        cmap = cmap,
+        dendrogram_ratio = (.1, .1),
+        annot_kws = {"size": 4},
+        figsize = (5,3.8),
+        cbar_kws = {"label": f"{corr_method} correlation",
+                    "orientation": 'horizontal'},
+        yticklabels = False,
+        xticklabels = False,
+        square = True)
     clustermap.fig.subplots_adjust(right=0.7)
 
     clustermap.ax_cbar.set_position([0.16, 0, 0.53, 0.02])
