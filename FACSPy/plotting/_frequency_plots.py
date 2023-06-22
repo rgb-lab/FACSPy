6accb46595cd2190c2304287f7c636b7b6b4a26d
TarikExner <Tarik.Exner@gmail.com>
2023-06-22 13:06:14 +0200

    added stacked bar frequency plot for cluster freq

--- FACSPy/plotting/_frequency_plots.py
+++ FACSPy/plotting/_frequency_plots.py
@@ -28,19 +28,29 @@ def prep_dataframe_cluster_freq(adata: AnnData,
         group_sizes = dataframe.groupby(cluster_key).sum()
         dataframe = dataframe.groupby([cluster_key, groupby]).mean().div(group_sizes).reset_index()
     
-    return dataframe.groupby([cluster_key, "condition"], as_index = False)["count"].mean()\
-        .pivot(index = cluster_key,
-               columns = groupby,
-               values = "count")
+    return dataframe.groupby([cluster_key, groupby], as_index = False)["count"].mean()\
+          .pivot(index = cluster_key,
+                 columns = groupby,
+                 values = "count")
 
+def order_dataframe(dataframe: pd.DataFrame,
+                    order: Union[str, list[str]]) -> pd.DataFrame:
+    return dataframe[order]
 
 def cluster_frequency(adata: AnnData,
                       groupby: Union[str, list[str]],
+                      order: Optional[list[str]] = None,
                       cluster_key: str = "leiden",
-                      normalize: bool = True) -> Optional[Figure]:
+                      normalize: bool = True,
+                      return_dataframe: bool = False) -> Optional[Figure]:
     
     dataframe = prep_dataframe_cluster_freq(adata, groupby, cluster_key, normalize)
-    print(dataframe.shape)
+    
+    if order is not None:
+        dataframe = order_dataframe(dataframe, order)
+
+    if return_dataframe:
+        return dataframe
     fig, ax = plt.subplots(ncols = 1, nrows = 1, figsize = (5,4))
     dataframe.plot(kind = "bar",
                    stacked = True,
@@ -49,5 +59,6 @@ def cluster_frequency(adata: AnnData,
               bbox_to_anchor = (1,1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 0)
     ax.set_title(f"{cluster_key} cluster frequency\nper {groupby}")
+    ax.set_ylabel("frequency [dec.]")
     plt.tight_layout()
     plt.show()
