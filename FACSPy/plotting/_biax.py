c720f3259f6868d100b9c1bab897f3c3c7bdfad6
TarikExner <Tarik.Exner@gmail.com>
2023-06-26 18:39:23 +0200

    added title and sampleID-not-found error

--- FACSPy/plotting/_biax.py
+++ FACSPy/plotting/_biax.py
@@ -11,7 +11,7 @@ from anndata import AnnData
 
 from typing import Union, Optional, Literal
 
-
+#TODO: add legend
 def biax(adata: AnnData,
          x_channel: str,
          y_channel: str,
@@ -21,23 +21,32 @@ def biax(adata: AnnData,
     
     if not isinstance(sample_ID, list):
         sample_ID = [sample_ID]
+    
+    for sample in sample_ID:
+        if sample not in adata.obs["sample_ID"].unique():
+            raise ValueError(f"sampleID {sample} not found!")
+    
     dataframe = adata[adata.obs["sample_ID"].isin(sample_ID)].to_df(layer = layer)
     dataframe["sample_ID"] = adata[adata.obs["sample_ID"].isin(sample_ID)].obs["sample_ID"].to_list()
 
-    fig, ax = plt.subplots(ncols = 1, nrows = 1, figsize = (5,5))
+    fig, ax = plt.subplots(ncols = 1, nrows = 1, figsize = (3,3))
     plot_params = {
         "data": dataframe,
         "x": x_channel,
         "y": y_channel,
         "linewidth": 0,
         "s": 2,
-        "c": dataframe["sample_ID"]
+        "c": dataframe["sample_ID"],
+        "legend": "auto"
     }
     sns.scatterplot(**plot_params,
                     ax = ax)
 
-    if layer == "compensated":
-        plt.xscale("symlog", linthresh = 5)
-        plt.yscale("symlog", linthresh = 5)
+    if layer in ["compensated", "raw"]:
+        plt.xscale("symlog",
+                   linthresh = adata.uns["cofactors"].get_cofactor(adata.var.loc[adata.var["pns"] == x_channel, "pnn"].iloc[0]))
+        plt.yscale("symlog",
+                   linthresh = adata.uns["cofactors"].get_cofactor(adata.var.loc[adata.var["pns"] == y_channel, "pnn"].iloc[0]))
+    ax.set_title(f"{layer}\nexpression")
     plt.tight_layout()
     plt.show()
