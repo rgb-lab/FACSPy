0f9dc61be11c0d15ac2d0679ad548da6f1c04f26
TarikExner <Tarik.Exner@gmail.com>
2023-07-02 14:26:01 +0200

    added dataset as dt

--- FACSPy/__init__.py
+++ FACSPy/__init__.py
@@ -3,10 +3,11 @@
 from . import tools as tl
 from . import plotting as pl
 from . import model as ml
+from . import dataset as dt
 
 from .utils import subset_gate, subset_fluo_channels, remove_unnamed_channels, equalize_groups
 from .io.io import save_dataset, read_dataset
 
 import sys
 
-sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['tl', 'pl', 'ml']})
+sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['tl', 'pl', 'ml', 'dt']})
