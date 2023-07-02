7689456069916acc64e34f8c1a8fcd70ae28fe6c
TarikExner <Tarik.Exner@gmail.com>
2023-07-02 12:49:26 +0200

    added aliases

---
+++ FACSPy/plotting/__init__.py
@@ -0,0 +1,11 @@
+from ._biax import biax
+from ._fold_change import fold_change
+from ._frequency_plots import cluster_frequency
+from ._marker_expressions import marker_density
+from ._mfi import mfi
+from ._sample_correlations import sample_correlation, marker_correlation
+from ._sample_distance import sample_distance
+from ._samplewise_dr import pca_samplewise, mds_samplewise
+from .cofactor_plots import cofactor_distribution, transformation_plot
+from .gating_strategy import gating_strategy
+from .qc import cell_counts, gate_frequency
