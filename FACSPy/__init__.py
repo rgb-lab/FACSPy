06f50a40bdb537d84e181bbbd66cc5cf5de1a0f7
TarikExner <Tarik.Exner@gmail.com>
2023-07-02 14:18:02 +0200

    added io and utils imports

--- FACSPy/__init__.py
+++ FACSPy/__init__.py
@@ -4,6 +4,9 @@ from . import tools as tl
 from . import plotting as pl
 from . import model as ml
 
+from .utils import subset_gate, subset_fluo_channels, remove_unnamed_channels, equalize_groups
+from .io.io import save_dataset, read_dataset
+
 import sys
 
 sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['tl', 'pl', 'ml']})
