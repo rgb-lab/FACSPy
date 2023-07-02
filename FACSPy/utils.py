26f63fe7e81597b32a110532dae597b9d35fac7c
TarikExner <Tarik.Exner@gmail.com>
2023-07-02 17:32:57 +0200

    implemented sanity checks if gate hierarchy is exhausted

--- FACSPy/utils.py
+++ FACSPy/utils.py
@@ -4,7 +4,7 @@ import warnings
 import numpy as np
 import pandas as pd
 
-from .exceptions.exceptions import ChannelSubsetError
+from .exceptions.exceptions import ChannelSubsetError, ExhaustedHierarchyError
 
 from itertools import combinations
 
@@ -43,15 +43,22 @@ def find_gate_indices(adata: AnnData,
         gate_columns = [gate_columns]
     return [adata.uns["gating_cols"].get_loc(gate) for gate in gate_columns]
 
+
 def find_parent_gate(gate: str) -> str:
     """returns the parent gate path"""
     """Example: gate = 'root/singlets/T_cells' -> 'root/singlets' """
-    return GATE_SEPARATOR.join(gate.split(GATE_SEPARATOR)[:-1])
+    if GATE_SEPARATOR in gate:
+        return GATE_SEPARATOR.join(gate.split(GATE_SEPARATOR)[:-1])
+    else:
+        raise ExhaustedHierarchyError(gate)
 
 def find_parent_population(gate: str) -> str:
     """returns the parent population"""
     """Example: gate = 'root/singlets/T_cells' -> 'singlets'"""
-    return gate.split(GATE_SEPARATOR)[:-1][::-1][0]
+    if GATE_SEPARATOR in gate:
+        return gate.split(GATE_SEPARATOR)[:-1][::-1][0]
+    else:
+        raise ExhaustedHierarchyError(gate)
 
 def find_grandparent_gate(gate: str) -> str:
     return find_parent_gate(find_parent_gate(gate))
