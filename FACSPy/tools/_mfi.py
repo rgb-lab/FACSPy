2414a734d2484d0890062d17816ad59d36f9e28b
TarikExner <Tarik.Exner@gmail.com>
2023-07-03 14:00:46 +0200

    refactored, added mfi functionality for transformed data as adata.uns['tmfi']

--- FACSPy/tools/_mfi.py
+++ FACSPy/tools/_mfi.py
@@ -1,36 +1,57 @@
 from typing import Union, Optional, Literal
-import anndata as ad
+from anndata import AnnData
 import numpy as np
 import pandas as pd
 
-def mfi(dataset: ad.AnnData,
+def calculate_medians(adata: AnnData,
+                      gates: Union[pd.Index, list[str]],
+                      on: Literal["raw", "compensated", "transformed"]) -> dict[str, np.ndarray]:
+    return {
+        gate: np.median(adata[adata.obsm["gating"][:,i] == 1,:].layers[on],
+                        axis = 0) 
+        for i, gate in enumerate(gates)
+    }
+
+def reindex_dictionary(dictionary: dict) -> dict:
+    ### reindexing the dictionary for multi-index in pandas    
+    return {(outer_key, inner_key): values
+            for outer_key, inner_dict in dictionary.items()
+            for inner_key, values in inner_dict.items()}
+
+def convert_to_dataframe(dictionary: dict,
+                         adata: AnnData) -> pd.DataFrame:
+    return pd.DataFrame(
+            data = dictionary,
+            index = adata.var.index,
+            dtype = np.float32
+        )
+
+def calculate_mfis(adata: AnnData,
+                   gates: Union[pd.Index, list[str]],
+                   on = Literal["raw", "compensated", "transformed"]) -> dict:
+    mfis = {}
+    for sample_id in adata.obs["sample_ID"].unique():
+        tmp = adata[adata.obs["sample_ID"] == sample_id, :]
+        mfis[str(sample_id)] = calculate_medians(adata = tmp,
+                                                 gates = gates,
+                                                 on = on)
+    return mfis
+
+def mfi(adata: AnnData,
         population: Optional[Union[list[str], str]] = None,
         on: Literal["raw", "compensated", "transformed"] = "compensated",
         copy: bool = False):
     
-    gates = dataset.uns["gating_cols"]
+    gates = adata.uns["gating_cols"]
 
-    mfis = {}
-    for sample_id in dataset.obs["sample_ID"].unique():
-        tmp = dataset[dataset.obs["sample_ID"] == sample_id, :]
-        mfis[str(sample_id)] = {
-                                    gate: np.median(tmp[tmp.obsm["gating"][:,i] == 1,:].layers[on],
-                                                    axis = 0) 
-                                    for i, gate in enumerate(gates)
-                                }
-    
-    ### reindexing the dictionary for multi-index in pandas    
-    mfis = {(outer_key, inner_key): values
-            for outer_key, inner_dict in mfis.items()
-            for inner_key, values in inner_dict.items()}
-    mfi_frame = pd.DataFrame(
-            data = mfis,
-            index = dataset.var.index,
-            dtype = np.float32
-        )
-    
-    mfi_frame.source = f"{on} events"
+    mfis = calculate_mfis(adata,
+                          gates,
+                          on = "compensated")
+    adata.uns["mfi"] = convert_to_dataframe(reindex_dictionary(mfis), adata)
 
-    dataset.uns["mfi"] = mfi_frame
+    tmfis = calculate_mfis(adata,
+                           gates,
+                           on = "transformed")
+    adata.uns["tmfi"] = convert_to_dataframe(reindex_dictionary(tmfis), adata)
 
-    return dataset if copy else None
+    return adata if copy else None
