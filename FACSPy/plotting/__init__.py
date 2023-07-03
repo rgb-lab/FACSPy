7538b420be3b04ac7dc0cf75f0f9009fde9b0950
TarikExner <Tarik.Exner@gmail.com>
2023-07-03 11:44:35 +0200

    added fop to aliases

--- FACSPy/plotting/__init__.py
+++ FACSPy/plotting/__init__.py
@@ -2,7 +2,7 @@ from ._biax import biax
 from ._fold_change import fold_change
 from ._frequency_plots import cluster_frequency
 from ._marker_expressions import marker_density
-from ._mfi import mfi
+from ._mfi import mfi, fop
 from ._sample_correlations import sample_correlation, marker_correlation
 from ._sample_distance import sample_distance
 from ._samplewise_dr import pca_samplewise, mds_samplewise
