98e05bb8efdf9c26ad80661a2c2d8e2f1917fd87
TarikExner <Tarik.Exner@gmail.com>
2023-07-03 23:05:38 +0200

    added cluster mfi plot alias

--- FACSPy/plotting/__init__.py
+++ FACSPy/plotting/__init__.py
@@ -10,4 +10,4 @@ from .cofactor_plots import cofactor_distribution, transformation_plot
 from .gating_strategy import gating_strategy
 from .qc import cell_counts, gate_frequency
 from ._expression_heatmap import expression_heatmap
-from ._cluster_mfi import cluster_heatmap
+from ._cluster_mfi import cluster_heatmap, cluster_mfi
