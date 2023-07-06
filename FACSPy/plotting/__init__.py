42d9d09df8e7b288ebb6b1e45f0f401cc0ef0c59
TarikExner <Tarik.Exner@gmail.com>
2023-07-06 12:46:43 +0200

    fixed aliases for marker_correlation

--- FACSPy/plotting/__init__.py
+++ FACSPy/plotting/__init__.py
@@ -3,7 +3,7 @@ from ._fold_change import fold_change
 from ._frequency_plots import cluster_frequency
 from ._marker_expressions import marker_density
 from ._mfi import mfi, fop
-from ._sample_correlations import sample_correlation, marker_correlation
+from ._sample_correlations import sample_correlation
 from ._sample_distance import sample_distance
 from ._samplewise_dr import pca_samplewise, mds_samplewise
 from .cofactor_plots import cofactor_distribution, transformation_plot
