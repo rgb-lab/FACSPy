c948709ec274ab5c079b851cbff523c4e121d260
TarikExner <Tarik.Exnerq@gmail.com>
2023-07-06 22:58:36 +0200

    moved HierarchyExhaustedError to new file

--- FACSPy/exceptions/exceptions.py
+++ FACSPy/exceptions/exceptions.py
@@ -47,15 +47,6 @@ class NotSupportedStatisticalTestError(Exception):
         )
         super().__init__(self.message)
 
-class ExhaustedHierarchyError(Exception):
-
-    def __init__(self,
-                 gate):
-        self.message = (
-            f"You have reached the maximum of the gating hierarchy. No Parent could be defined for {gate}"
-        )
-        super().__init__(self.message)
-
 class HierarchyError(Exception):
 
     def __init__(self):
