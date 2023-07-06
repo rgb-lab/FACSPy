d099383443b734392f504a3a1039c153879d2124
TarikExner <Tarik.Exner@gmail.com>
2023-07-06 11:42:59 +0200

    changed data import to global get_uns_dataframe

--- FACSPy/plotting/_expression_heatmap.py
+++ FACSPy/plotting/_expression_heatmap.py
@@ -1,23 +1,17 @@
 from anndata import AnnData
 from matplotlib import pyplot as plt
-import seaborn as sns
 import numpy as np
 import pandas as pd
 
-from matplotlib.axes import Axes
 from matplotlib.figure import Figure
-
-from matplotlib.patches import Patch
 
 from typing import Literal, Optional, Union
 
-from ..utils import subset_gate, find_gate_path_of_gate
-from .utils import (prep_uns_dataframe,
-                    scale_data,
-                    select_gate_from_singleindex_dataframe,
+from .utils import (scale_data,
                     map_obs_to_cmap,
                     calculate_sample_distance,
                     calculate_linkage,
+                    append_metadata,
                     add_metaclusters,
                     remove_ticklabels,
                     remove_ticks,
@@ -26,17 +20,21 @@ from .utils import (prep_uns_dataframe,
                     calculate_correlation_data,
                     remove_dendrogram,
                     add_annotation_plot,
+                    get_uns_dataframe,
                     ANNOTATION_CMAPS)
 
 from ._clustermap import create_clustermap
 
-from scipy.spatial import distance
-from scipy.cluster import hierarchy
-from scipy.spatial import distance_matrix
-
-
-
-from ..exceptions.exceptions import AnalysisNotPerformedError
+def prepare_plot_data(adata: AnnData,
+                      raw_data: pd.DataFrame,
+                      scaling: Optional[Literal["MinMaxScaler", "RobustScaler"]],
+                      copy: bool = False
+                      ) -> pd.DataFrame:
+    plot_data = raw_data.copy() if copy else raw_data
+    fluo_columns = [col for col in raw_data.columns if col in adata.var_names]
+    if scaling is not None:
+        plot_data[fluo_columns] = scale_data(plot_data[fluo_columns], scaling)
+    return plot_data
 
 def expression_heatmap(adata: AnnData,
                        groupby: Optional[Union[str, list[str]]],
@@ -46,65 +44,56 @@ def expression_heatmap(adata: AnnData,
                        on: Literal["mfi", "fop", "gate_frequency"] = "mfi",
                        corr_method: Literal["pearson", "spearman", "kendall"] = "pearson",
                        cluster_method: Literal["correlation", "distance"] = "distance",
-                       cmap: str = "inferno",
+                       cmap: str = "RdBu_r",
                        return_fig: bool = False,
                        metaclusters: Optional[int] = None,
                        label_metaclusters_in_dataset: bool = True,
                        label_metaclusters_key: Optional[str] = "metacluster_sc",
                        figsize: Optional[tuple[int, int]] = (5,3.8),
-                       y_label_fontsize: Optional[Union[int, float]] = 4) -> Optional[Figure]:
-    
-    try:
-        if on == "all_cells":
-            adata = subset_gate(adata, gate, as_view = True)
-            data = adata.to_df(layer = "transformed")
-
-        else:
-            data = adata.uns[on]
-            data = prep_uns_dataframe(adata, data)
-            data = select_gate_from_singleindex_dataframe(data, find_gate_path_of_gate(adata, gate))
-            annot_data = data.copy()
-        fluo_columns = [col for col in data.columns if col in adata.var_names.to_list()]
-        if scaling is not None:
-            data[fluo_columns] = scale_data(data[fluo_columns], scaling)
-    
-    except KeyError as e:
-        raise AnalysisNotPerformedError(on) from e
-
+                       y_label_fontsize: Optional[Union[int, float]] = 4,
+                       return_dataframe: bool = False) -> Optional[Figure]:
 
     if not isinstance(groupby, list):
-        groupby = [groupby]
+        groupby = [groupby]    
+    
+    raw_data = get_uns_dataframe(adata = adata,
+                                 gate = gate,
+                                 table_identifier = on,
+                                 column_identifier_name = "sample_ID")
+    fluo_columns = [col for col in raw_data.columns if col in adata.var_names]
+    plot_data = prepare_plot_data(adata = adata,
+                                  raw_data = raw_data,
+                                  copy = True,
+                                  scaling = scaling)
 
-    sample_IDs = data["sample_ID"].to_list()
-    sample_annot = data["sample_ID"].astype("object").to_list()
-    raw_data = data[fluo_columns].T
-    raw_data.columns = sample_annot
-
+    if return_dataframe:
+        return plot_data
+    
+    
     if cluster_method == "correlation":
-        correlation_data = raw_data.corr(method = corr_method)
-        row_linkage = calculate_linkage(correlation_data.T)
+        col_linkage = calculate_linkage(calculate_correlation_data(plot_data[fluo_columns].T, corr_method))
     
     elif cluster_method == "distance":
-        distance_data = calculate_sample_distance(data[fluo_columns])
-        row_linkage = calculate_linkage(distance_data)
+        col_linkage = calculate_linkage(calculate_sample_distance(plot_data[fluo_columns]))
 
     if metaclusters is not None:
         groupby += ["metacluster"]
-        data = add_metaclusters(adata = adata,
-                                data = data,
-                                row_linkage = row_linkage,
-                                n_clusters = metaclusters,
-                                sample_IDs = sample_IDs,
-                                label_metaclusters = label_metaclusters_in_dataset,
-                                label_metaclusters_key = label_metaclusters_key)
+        plot_data = add_metaclusters(adata = adata,
+                                     data = plot_data,
+                                     row_linkage = col_linkage,
+                                     n_clusters = metaclusters,
+                                     sample_IDs = raw_data.index,
+                                     label_metaclusters = label_metaclusters_in_dataset,
+                                     label_metaclusters_key = label_metaclusters_key)
 
-    clustermap = create_clustermap(data = raw_data,
+    ### for the heatmap, the dataframe is transposed so that sample_IDs are the columns
+    clustermap = create_clustermap(data = plot_data[fluo_columns].T,
                                    col_colors = [
-                                       map_obs_to_cmap(data, group, ANNOTATION_CMAPS[i])
+                                       map_obs_to_cmap(plot_data, group, ANNOTATION_CMAPS[i])
                                        for i, group in enumerate(groupby)
                                    ],
                                    row_cluster = True,
-                                   col_linkage = row_linkage,
+                                   col_linkage = col_linkage,
                                    cmap = cmap,
                                    figsize = figsize,
                                    cbar_kws = {"label": "scaled expression", "orientation": 'horizontal'},
@@ -121,6 +110,7 @@ def expression_heatmap(adata: AnnData,
     remove_ticklabels(ax, which = "x")
     remove_ticks(ax, which = "x")
     remove_dendrogram(clustermap, which = "y")
+    ax.set_xlabel("")
 
     ax.yaxis.set_ticks_position("left")
     ax.set_yticklabels(ax.get_yticklabels(), fontsize = y_label_fontsize)
@@ -128,15 +118,12 @@ def expression_heatmap(adata: AnnData,
 
     add_categorical_legend_to_clustermap(clustermap,
                                          heatmap = ax,
-                                         data = data,
+                                         data = plot_data,
                                          groupby = groupby,)
 
     if annotate is not None:
         if annotate in adata.var_names:
-            annot_data["sample_ID"] = annot_data["sample_ID"].astype("object")
-            annot_data = annot_data.set_index("sample_ID")
-            annot_data.index = [str(idx) for idx in annot_data.index.to_list()]
-            annot_frame = annot_data[annotate]
+            annot_frame = raw_data[annotate]
         
         add_annotation_plot(adata = adata,
                             annotate = annotate,
