8b51d90cdb00104f3e0ea75881b384d318d4b5b8
TarikExner <Tarik.Exnerq@gmail.com>
2023-07-06 22:59:05 +0200

    added GateNotProvidedError, moved ExhaustedHierarchyError

---
+++ FACSPy/exceptions/utils.py
@@ -0,0 +1,20 @@
+
+class GateNotProvidedError(Exception):
+
+    def __init__(self,
+                 gate):
+        self.message = (
+                f"No gate was provided to the function. The input was {gate}"
+        )
+        super().__init__(self.message)
+
+class ExhaustedHierarchyError(Exception):
+
+    def __init__(self,
+                 gate):
+        self.message = (
+            f"You have reached the maximum of the gating hierarchy. No Parent could be defined for {gate}"
+        )
+        super().__init__(self.message)
+
+
