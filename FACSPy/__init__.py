c37ea8a42a7c21d9062bdf4ed4c75888612e0cac
TarikExner <Tarik.Exner@gmail.com>
2023-07-06 17:19:03 +0200

    added aliases for convert_gates_to_obs

--- FACSPy/__init__.py
+++ FACSPy/__init__.py
@@ -5,7 +5,12 @@ from . import plotting as pl
 from . import model as ml
 from . import dataset as dt
 
-from .utils import subset_gate, subset_fluo_channels, remove_unnamed_channels, equalize_groups
+from .utils import (subset_gate,
+                    subset_fluo_channels,
+                    remove_unnamed_channels,
+                    equalize_groups,
+                    convert_gate_to_obs,
+                    convert_gates_to_obs)
 from .io.io import save_dataset, read_dataset
 
 import sys
