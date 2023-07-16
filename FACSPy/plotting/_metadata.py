6ae918efc57111428c4792339ad03a4bda48f694
TarikExner <Tarik.Exner@gmail.com>
2023-07-16 12:35:17 +0200

    major rewrite, consistent with other plots now

--- FACSPy/plotting/_metadata.py
+++ FACSPy/plotting/_metadata.py
@@ -5,13 +5,21 @@ from matplotlib import pyplot as plt
 
 from anndata import AnnData
 
-from .utils import create_boxplot, savefig_or_show
+from .utils import savefig_or_show
+
+from ._baseplot import (stripboxplot,
+                        barplot,
+                        label_plot_basic,
+                        adjust_legend)
+from ._basestats import add_statistic
 
 from typing import Optional
 
 def metadata(adata: AnnData,
              marker: str,
              groupby: str,
+             colorby: str,
+             figsize: tuple[float, float] = (3,3),
              return_dataframe: bool = False,
              return_fig: bool = False,
              save: Optional[str] = None,
@@ -20,6 +28,9 @@ def metadata(adata: AnnData,
     if not isinstance(groupby, list):
         groupby = [groupby]
     
+    if not isinstance(colorby, list):
+        colorby = [colorby]
+    
     
     df = adata.uns["metadata"].dataframe.copy()
 
@@ -27,30 +38,49 @@ def metadata(adata: AnnData,
     if return_dataframe:
         return df
     
-    ncols = 1,
+    ncols = 1
     nrows = len(groupby)
-    figsize = (len(df) / 10, 3 * len(groupby)) 
-    
-    fig, ax = plt.subplots(ncols = 1, nrows = nrows, figsize = figsize)
-    for i, grouping in enumerate(groupby):
-        plot_params = {
-            "data": df,
-            "x": "sample_ID" if grouping is None else grouping,
-            "y": marker,
+    figsize = figsize
+    plot_params = {
+        "data": df,
+        "x": groupby[0],
+        "y": marker,
+        "hue": colorby[0],
 
-        }
-        if len(groupby) > 1:
-            ax[i] = create_boxplot(ax[i],
-                                   grouping,
-                                   plot_params)
-        else:
-            ax = create_boxplot(ax,
-                                grouping,
-                                plot_params)
+    }
+
+    fig, ax = plt.subplots(ncols = ncols, nrows = nrows, figsize = figsize)
+    if groupby == ["sample_ID"]:
+        ax = barplot(ax,
+                     plot_params = plot_params)
+
+    else:
+        ax = stripboxplot(ax,
+                          plot_params = plot_params)
+        try:
+            ax = add_statistic(ax = ax,
+                                test = "Kruskal",
+                                dataframe = df,
+                                groupby = groupby[0],
+                                plot_params = plot_params)
+        except ValueError as e:
+            if str(e) != "All numbers are identical in kruskal":
+                raise ValueError from e
+            else:
+                print("warning... Values were uniform, no statistics to plot.")
+
+    ax = label_plot_basic(ax = ax,
+                          title = f"{marker}\ngrouped by {groupby[0]}",
+                          y_label = f"{marker}",
+                          x_label = "")
+    
+    ax = adjust_legend(ax)
+    
+    ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = "center")
         
     if return_fig:
         return fig
-    
+
     plt.tight_layout()
     savefig_or_show(save = save, show = show)
     
