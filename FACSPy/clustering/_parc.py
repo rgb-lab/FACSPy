18c27f20370c40c68b441012d7d45c82a5827dfe
TarikExner <Tarik.Exner@gmail.com>
2023-07-22 09:43:31 +0200

    implemented parc clustering

---
+++ FACSPy/clustering/_parc.py
@@ -0,0 +1,33 @@
+import parc
+
+from anndata import AnnData
+
+from typing import Optional, Union, Literal
+
+from ..utils import contains_only_fluo
+
+def parc_cluster(adata: AnnData,
+                 on: Literal["compensated", "transformed"] = "transformed",
+                 exclude: Optional[Union[str, list[str]]] = None,
+                 copy: bool = False,
+                 parc_kwargs=None) -> Optional[AnnData]:
+    
+    if parc_kwargs is None:
+        parc_kwargs = {}
+    cluster_set = adata.copy() if copy else adata
+
+    assert contains_only_fluo(cluster_set)
+
+    if exclude:
+        cluster_set = adata[:, [var for var in adata.var_names if var not in exclude]]
+        assert adata.isview
+
+    ### we take the raw data as they are mostly below 50 markers anyway and would probably be not too much higher
+    parcer = parc.PARC(cluster_set.layers[on],
+                       neighbor_graph = adata.obsp["connectivities"] if "connectivities" in adata.obsp else None,
+                       **parc_kwargs)
+    parcer.run_PARC()
+    adata.obs["parc_labels"] = parcer.labels
+    adata.obs["parc_labels"] = adata.obs["parc_labels"].astype("category")
+
+    return adata if copy else None
