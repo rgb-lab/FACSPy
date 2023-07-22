7e1d64d6f323b9057e1005b74f52cc3db4a43ecb
TarikExner <Tarik.Exner@gmail.com>
2023-07-22 09:43:12 +0200

    added wrapper around sc.tl.leiden

--- FACSPy/clustering/_leiden.py
+++ FACSPy/clustering/_leiden.py
@@ -2,16 +2,33 @@
 from ..utils import subset_fluo_channels
 
 import scanpy as sc
-import anndata as ad
+from anndata import AnnData
 
-from typing import Optional#
+from typing import Optional, Union, Literal
 
-from scanpy.preprocessing import neighbors
+from ..utils import contains_only_fluo
 
-from scanpy.tools import umap
 
-def leiden(dataset: ad.AnnData,
-           copy: bool = False) -> Optional[ad.AnnData]:
+def leiden_cluster(adata: AnnData,
+                   on: Literal["compensated", "transformed"] = "transformed",
+                   exclude: Optional[Union[str, list[str]]] = None,
+                   copy: bool = False,
+                   leiden_kwargs=None) -> Optional[AnnData]:
     
-    data = subset_fluo_channels(dataset)
-    return sc.pp.neighbors(dataset)
+    if leiden_kwargs is None:
+        leiden_kwargs = {}
+    cluster_set = adata.copy() if copy else adata
+
+    assert contains_only_fluo(cluster_set)
+
+    if exclude:
+        cluster_set = adata[:, [var for var in adata.var_names if var not in exclude]]
+        assert adata.isview
+
+    ### we take the raw data as they are mostly below 50 markers anyway and would probably be not too much higher
+    sc.tl.leiden(cluster_set,
+                 **leiden_kwargs)
+
+    adata.obs["leiden"] = cluster_set.obs["leiden"]
+
+    return adata if copy else None
