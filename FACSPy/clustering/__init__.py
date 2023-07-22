1107d15b89c51673599c90b2928b92730c64d16a
TarikExner <Tarik.Exner@gmail.com>
2023-07-22 09:42:41 +0200

    added aliases

--- FACSPy/clustering/__init__.py
+++ FACSPy/clustering/__init__.py
@@ -1,3 +1,4 @@
 from ._flowsom import flowsom
-from ._leiden import leiden
-from ._phenograph import phenograph
+from ._leiden import leiden_cluster as leiden
+from ._phenograph import phenograph_cluster as phenograph
+from ._parc import parc_cluster as parc
