fa6869f7f93cbb0e47320640b1245eb060fc7b43
TarikExner <Tarik.Exnerq@gmail.com>
2023-08-05 16:48:55 +0200

    added specs!

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
 setuptools.setup(name='FACSPy',
-version='0.11',
-description='An example package',
-url='#',
-author='max',
+version='0.0.1',
+description='Automated Flow Cytometry Analysis',
+url='www.google.de',
+author='Tarik Exner',
 install_requires=[],
-author_email='',
+author_email='Tarik.Exner@med.uni-heidelberg',
 packages=setuptools.find_packages(),
 zip_safe=False)
