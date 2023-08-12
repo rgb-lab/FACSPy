a548403ddcf6a0fa6c1cd53f0e5761116c6c018c
TarikExner <Tarik.Exnerq@gmail.com>
2023-08-12 16:24:17 +0200

    added support for flowsom

--- FACSPy/clustering/_flowsom.py
+++ FACSPy/clustering/_flowsom.py
@@ -1,172 +1,59 @@
-### copied from github.com/Hatchin/FlowSOM
-
 from typing import Optional, Literal
 
-import anndata as ad
-import numpy as np
-import pandas as pd
+from anndata import AnnData
+from typing import Union
+from ..utils import contains_only_fluo
 
-from itertools import combinations
-import bisect
+from FlowSOM.cluster import flowsom
 
-#from minisom import MiniSom
-from ..utils import subset_fluo_channels
-
-
-class ConsensusCluster:
-    """
-      Implementation of Consensus clustering, following the paper
-      https://link.springer.com/content/pdf/10.1023%2FA%3A1023949509487.pdf
-      Args:
-      copied from https://github.com/Hatchin/FlowSOM/blob/master/script/cluster.py
-        * cluster -> clustering class
-                    needs fit_predict method called with parameter n_clusters
-        * L -> smallest number of clusters to try
-        * K -> biggest number of clusters to try
-        * H -> number of resamplings for each cluster number
-        * resample_proportion -> percentage to sample
-        * Mk -> consensus matrices for each k (shape =(K,data.shape[0],data.shape[0]))
-                (NOTE: every consensus matrix is retained, like specified in the paper)
-        * Ak -> area under CDF for each number of clusters 
-                (see paper: section 3.3.1. Consensus distribution.)
-        * deltaK -> changes in ares under CDF
-                (see paper: section 3.3.1. Consensus distribution.)
-        * self.bestK -> number of clusters that was found to be best
-      """
-
-    def __init__(self, cluster, L, K, H, resample_proportion=0.5):
-        assert 0 <= resample_proportion <= 1, "proportion has to be between 0 and 1"
-        self.cluster_ = cluster
-        self.resample_proportion_ = resample_proportion
-        self.L_ = L
-        self.K_ = K
-        self.H_ = H
-        self.Mk = None
-        self.Ak = None
-        self.deltaK = None
-        self.bestK = None
-
-    def _internal_resample(self, data, proportion):
-        """
-        Args:
-          * data -> (examples,attributes) format
-          * proportion -> percentage to sample
-        """
-        resampled_indices = np.random.choice(
-            range(data.shape[0]), size=int(data.shape[0]*proportion), replace=False)
-        return resampled_indices, data[resampled_indices, :]
-
-    def fit(self, data, verbose=False):
-        """
-        Fits a consensus matrix for each number of clusters
-        Args:
-          * data -> (examples,attributes) format
-          * verbose -> should print or not
-        """
-        Mk = np.zeros((self.K_-self.L_, data.shape[0], data.shape[0]))
-        Is = np.zeros((data.shape[0],)*2)
-        for k in range(self.L_, self.K_):  # for each number of clusters
-            i_ = k-self.L_
-            if verbose:
-                print("At k = %d, aka. iteration = %d" % (k, i_))
-            for h in range(self.H_):  # resample H times
-                if verbose:
-                    print("\tAt resampling h = %d, (k = %d)" % (h, k))
-                resampled_indices, resample_data = self._internal_resample(
-                    data, self.resample_proportion_)
-                Mh = self.cluster_(n_clusters=k).fit_predict(resample_data)
-                # find indexes of elements from same clusters with bisection
-                # on sorted array => this is more efficient than brute force search
-                id_clusts = np.argsort(Mh)
-                sorted_ = Mh[id_clusts]
-                for i in range(k):  # for each cluster
-                    ia = bisect.bisect_left(sorted_, i)
-                    ib = bisect.bisect_right(sorted_, i)
-                    is_ = id_clusts[ia:ib]
-                    ids_ = np.array(list(combinations(is_, 2))).T
-                    # sometimes only one element is in a cluster (no combinations)
-                    if ids_.size != 0:
-                        Mk[i_, ids_[0], ids_[1]] += 1
-                # increment counts
-                ids_2 = np.array(list(combinations(resampled_indices, 2))).T
-                Is[ids_2[0], ids_2[1]] += 1
-            Mk[i_] /= Is+1e-8  # consensus matrix
-            # Mk[i_] is upper triangular (with zeros on diagonal), we now make it symmetric
-            Mk[i_] += Mk[i_].T
-            Mk[i_, range(data.shape[0]), range(
-                data.shape[0])] = 1  # always with self
-            Is.fill(0)  # reset counter
-        self.Mk = Mk
-        # fits areas under the CDFs
-        self.Ak = np.zeros(self.K_-self.L_)
-        for i, m in enumerate(Mk):
-            hist, bins = np.histogram(m.ravel(), density=True)
-            self.Ak[i] = np.sum(h*(b-a)
-                             for b, a, h in zip(bins[1:], bins[:-1], np.cumsum(hist)))
-        # fits differences between areas under CDFs
-        self.deltaK = np.array([(Ab-Aa)/Aa if i > 2 else Aa
-                                for Ab, Aa, i in zip(self.Ak[1:], self.Ak[:-1], range(self.L_, self.K_-1))])
-        self.bestK = np.argmax(self.deltaK) + \
-            self.L_ if self.deltaK.size > 0 else self.L_
-
-    def predict(self):
-        """
-        Predicts on the consensus matrix, for best found cluster number
-        """
-        assert self.Mk is not None, "First run fit"
-        return self.cluster_(n_clusters=self.bestK).fit_predict(
-            1-self.Mk[self.bestK-self.L_])
-
-    def predict_data(self, data):
-        """
-        Predicts on the data, for best found cluster number
-        Args:
-          * data -> (examples,attributes) format 
-        """
-        assert self.Mk is not None, "First run fit"
-        return self.cluster_(n_clusters=self.bestK).fit_predict(
-            data)
-
-
-def flowsom(dataset: ad.AnnData,
-            metaclustering_algorithm,
-            x: int,
-            y: int,
-            sigma: float,
-            learning_rate: float,
-            batch_size: int,
-            neighborhood_function: Literal["gaussian"],
-            key_added: str = "flowsom_meta",
-            random_state: int = 42,
-            copy: bool = False) -> Optional[ad.AnnData]:
+def flowsom_cluster(adata: AnnData,
+                    on: Literal["compensated", "transformed"] = "transformed",
+                    exclude: Optional[Union[str, list[str]]] = None,
+                    copy: bool = False,
+                    x_dim: int = 50,
+                    y_dim: int = 50,
+                    sigma: float = 1,
+                    learning_rate: float = 0.5,
+                    n_iterations: int = 100,
+                    neighborhood_function = "gaussian",
+                    consensus_cluster_algorithm: str = "AgglomerativeClustering",
+                    consensus_cluster_min_n: int = 10,
+                    consensus_cluster_max_n: int = 50,
+                    consensus_cluster_resample_proportion: float = 0.5,
+                    consensus_cluster_n_resamples: int = 10,
+                    verbose: bool = False,
+                    n_jobs: int = None,
+                    random_state: int = 187) -> Optional[AnnData]:
     
-    pass
-    # data = subset_fluo_channels(dataset)
+    if parc_kwargs is None:
+        parc_kwargs = {}
+    cluster_set = adata.copy() if copy else adata
 
-    # som = MiniSom(x = x,
-    #                 y = y,
-    #                 input_len = data.shape[1],
-    #                 sigma = sigma,
-    #                 learning_rate = learning_rate,
-    #                 neighborhood_function = neighborhood_function,
-    #                 random_seed = random_state
-    #                 )
-    # som.pca_weights_init(data)
-    # som.train_batch(data, batch_size, verbose = True)
-    # weights = som.get_weights().reshape(x * y, data.shape[1])
+    assert contains_only_fluo(cluster_set)
 
-    # for meta_cluster in [] 
-    # cluster = ConsensusCluster(metaclustering_algorithm,
-    #                            meta_cluster,
-    #                            meta_cluster + 1,
-    #                            3,
-    #                            resample_proportion = 0.5)
-    
-    # cluster.fit(weights, verbose = True)
-    # map_class = cluster.predict_data(weights).reshape(x, y)
-    # dataset.obs[key_added] = [map_class[som.winner(data[row,:])] for row in range(data.shape[0])]
+    if exclude:
+        cluster_set = adata[:, [var for var in adata.var_names if var not in exclude]]
+        assert adata.isview
 
+    ### we take the raw data as they are mostly below 50 markers anyway and would probably be not too much higher
+    adata.obs["flowsom_labels"] = flowsom(adata.layers[on],
+                                          x_dim = x_dim,
+                                          y_dim = y_dim,
+                                          sigma = sigma,
+                                          learning_rate = learning_rate,
+                                          n_iterations = n_iterations,
+                                          neighborhood_function = neighborhood_function,
+                                          consensus_cluster_algorithm = consensus_cluster_algorithm,
+                                          consensus_cluster_min_n = consensus_cluster_min_n,
+                                          consensus_cluster_max_n = consensus_cluster_max_n,
+                                          consensus_cluster_n_resamples = consensus_cluster_n_resamples,
+                                          consensus_cluster_resample_proportion = consensus_cluster_resample_proportion,
+                                          verbose = verbose,
+                                          random_state = random_state,
+                                          n_jobs = n_jobs)
+    adata.obs["flowsom_labels"] = adata.obs["flowsom_labels"].astype("category")
 
+    return adata if copy else None
 
 
 
