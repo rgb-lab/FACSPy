788637126f0d136b0ef03ecdcccdf291b5fac328
TarikExner <Tarik.Exnerq@gmail.com>
2023-08-13 19:52:54 +0200

    added kwarg support

--- FACSPy/clustering/_parc.py
+++ FACSPy/clustering/_parc.py
@@ -10,10 +10,10 @@ def parc_cluster(adata: AnnData,
                  on: Literal["compensated", "transformed"] = "transformed",
                  exclude: Optional[Union[str, list[str]]] = None,
                  copy: bool = False,
-                 parc_kwargs=None) -> Optional[AnnData]:
+                 cluster_kwargs=None) -> Optional[AnnData]:
     
-    if parc_kwargs is None:
-        parc_kwargs = {}
+    if cluster_kwargs is None:
+        cluster_kwargs = {}
     cluster_set = adata.copy() if copy else adata
 
     assert contains_only_fluo(cluster_set)
@@ -25,7 +25,7 @@ def parc_cluster(adata: AnnData,
     ### we take the raw data as they are mostly below 50 markers anyway and would probably be not too much higher
     parcer = parc.PARC(cluster_set.layers[on],
                        neighbor_graph = adata.obsp["connectivities"] if "connectivities" in adata.obsp else None,
-                       **parc_kwargs)
+                       **cluster_kwargs)
     parcer.run_PARC()
     adata.obs["parc_labels"] = parcer.labels
     adata.obs["parc_labels"] = adata.obs["parc_labels"].astype("category")
