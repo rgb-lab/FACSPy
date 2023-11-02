a6970f1304d63ea5d3167e50323c3c520afa4e01
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-02 16:27:18 +0100

    updated imports

--- FACSPy/dataset/__init__.py
+++ FACSPy/dataset/__init__.py
@@ -1,3 +1,4 @@
-from .dataset import DatasetAssembler, Transformer
-from .supplements import CofactorTable, Metadata, Panel
-from .workspaces import FlowJoWorkspace, DivaWorkspace
+from ._dataset import DatasetAssembler, Transformer, create_dataset
+from ._transformation import transform
+from ._supplements import CofactorTable, Metadata, Panel
+from ._workspaces import FlowJoWorkspace, DivaWorkspace
