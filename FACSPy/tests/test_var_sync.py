afe8f35fdc9dba0e9e329b3371f8a40495366faa
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-09 16:22:40 +0100

    bug fix

--- FACSPy/tests/test_var_sync.py
+++ FACSPy/tests/test_var_sync.py
@@ -5,10 +5,10 @@ import numpy as np
 from anndata import AnnData
 import pandas as pd
 import FACSPy as fp
-from FACSPy.synchronization._var_sync import (synchronize_vars,
-                                              _get_samplewise_dimred_columns,
-                                              _get_present_samplewise_dimreds,
-                                              _get_frame_metrics)
+from FACSPy.synchronization._utils import (_get_frame_metrics,
+                                           _get_present_samplewise_dimreds,
+                                           _get_samplewise_dimred_columns)
+from FACSPy.synchronization._var_sync import synchronize_vars
 from FACSPy.synchronization._synchronize import _dataset_has_been_modified
 from FACSPy.dataset._supplements import Metadata, Panel
 from FACSPy.dataset._workspaces import FlowJoWorkspace
@@ -40,7 +40,6 @@ def mock_dataset():
     fp.tl.mds_samplewise(adata)
     return adata
 
-
 def test_synchronize_vars_wo_recalc(mock_dataset: AnnData):
     adata = mock_dataset
     df: pd.DataFrame = adata.uns["mfi_sample_ID_compensated"]
@@ -65,7 +64,12 @@ def test_synchronize_vars_with_recalc(mock_dataset: AnnData):
     df_after_sync: pd.DataFrame = adata.uns["mfi_sample_ID_compensated"]
     assert "MDS1" in df_after_sync.columns
     assert "CD15" not in df_after_sync.columns
-    np.not_equal(df[["MDS1", "MDS2", "MDS3"]].values.flatten(), df_after_sync[["MDS1", "MDS2", "MDS3"]].values.flatten())
+    # any needed because the last row of MDS coordinates is zero for all samples
+    assert np.any(
+        np.not_equal(df[["MDS1", "MDS2", "MDS3"]].values.flatten(),
+                     df_after_sync[["MDS1", "MDS2", "MDS3"]].values.flatten()
+        )
+    )
 
 def test_get_samplewise_dimred_columns(mock_dataset):
     adata = mock_dataset
@@ -95,6 +99,7 @@ def test_get_frame_metrics():
     assert data_group == "some_long_string_that_a_user_gave_the_group"
 
 def test_panel_update(mock_dataset: AnnData):
+    # cofactors need to be added manually here
     cofactors = CofactorTable(cofactors = pd.DataFrame(data = {"fcs_colname": mock_dataset.uns["panel"].dataframe["antigens"].to_list(),
                                                                "cofactors": [1 for _ in range(len(mock_dataset.uns["panel"].dataframe))]}))
     mock_dataset.uns["cofactors"] = cofactors
