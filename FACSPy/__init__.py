5bae4596195a13a20322a88212522c099802abe4
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-09 17:50:19 +0100

    code aesthetics

--- FACSPy/__init__.py
+++ FACSPy/__init__.py
@@ -6,17 +6,19 @@ from . import model as ml
 from . import dataset as dt
 from . import synchronization as sync
 
-from .utils import (subset_gate,
-                    subset_fluo_channels,
-                    remove_unnamed_channels,
-                    remove_channel,
-                    equalize_groups,
-                    convert_gate_to_obs,
-                    convert_gates_to_obs,
-                    add_metadata_to_obs,
-                    rename_channel,
-                    convert_cluster_to_gate)
-from .io.io import save_dataset, read_dataset
+
+from .dataset import create_dataset, transform
+from ._utils import (subset_gate,
+                     subset_fluo_channels,
+                     remove_unnamed_channels,
+                     remove_channel,
+                     equalize_groups,
+                     convert_gate_to_obs,
+                     convert_gates_to_obs,
+                     add_metadata_to_obs,
+                     rename_channel,
+                     convert_cluster_to_gate)
+from .io._io import save_dataset, read_dataset
 
 import sys
 
