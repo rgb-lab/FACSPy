99b1403096ce5f37b49425218d358aa004b5a01c
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-21 13:10:18 +0100

    refactored functions, added imports

--- FACSPy/tools/_dr.py
+++ FACSPy/tools/_dr.py
@@ -1,13 +1,250 @@
-import scanpy as sc
-
 from anndata import AnnData
 from typing import Literal, Optional
 
+from ._tsne import _tsne
+from ._pca import _pca
+from ._diffmap import _diffmap
+from ._umap import _umap
+from ._neighbors import _neighbors
 from ._utils import (_preprocess_adata,
-                     merge_pca_info_into_adata,
-                     merge_neighbors_info_into_adata,
-                     merge_dimred_coordinates_into_adata,
-                     add_uns_data)
+                     _extract_valid_pca_kwargs,
+                     _extract_valid_neighbors_kwargs,
+                     _extract_valid_tsne_kwargs,
+                     _extract_valid_umap_kwargs)
+
+from .._utils import IMPLEMENTED_SCALERS
+from ..exceptions._exceptions import InvalidScalingError
+
+
+def pca(adata: AnnData,
+        gate: str,
+        layer: Literal["compensated", "transformed"] = "transformed",
+        use_only_fluo: bool = True,
+        exclude: Optional[list[str]] = None,
+        scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
+        copy: bool = False,
+        **kwargs) -> Optional[AnnData]:
+
+    adata = adata.copy() if copy else adata
+
+    exclude = [] if exclude is None else exclude
+
+    if not scaling in IMPLEMENTED_SCALERS and scaling is not None:
+        raise InvalidScalingError(scaler = scaling)
+
+    _save_dr_settings(adata = adata,
+                      gate = gate,
+                      layer = layer,
+                      use_only_fluo = use_only_fluo,
+                      exclude = exclude,
+                      scaling = scaling,
+                      reduction = "pca",
+                      **kwargs)
+    
+    preprocessed_adata = _preprocess_adata(adata = adata,
+                                           gate = gate,
+                                           layer = layer,
+                                           use_only_fluo = use_only_fluo,
+                                           exclude = exclude,
+                                           scaling = scaling)
+    
+    uns_key = f"{gate}_{layer}"
+    dimred_key = f"pca_{uns_key}"
+
+    adata = _pca(adata = adata,
+                 preprocessed_adata = preprocessed_adata,
+                 dimred_key = dimred_key,
+                 **kwargs)    
+
+    del adata.X
+    return adata if copy else None
+
+def diffmap(adata: AnnData,
+            gate: str,
+            layer: Literal["compensated", "transformed"] = "transformed",
+            recalculate_pca: bool = False,
+            use_only_fluo: bool = True,
+            exclude: Optional[list[str]] = None,
+            scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
+            copy: bool = False,
+            *args,
+            **kwargs) -> Optional[AnnData]:
+
+    adata = adata.copy() if copy else adata
+
+    exclude = [] if exclude is None else exclude
+
+    if not scaling in IMPLEMENTED_SCALERS and scaling is not None:
+        raise InvalidScalingError(scaler = scaling)
+
+    _save_dr_settings(adata = adata,
+                      gate = gate,
+                      layer = layer,
+                      use_only_fluo = use_only_fluo,
+                      exclude = exclude,
+                      scaling = scaling,
+                      reduction = "pca",
+                      *args,
+                      **kwargs)
+    
+    uns_key = f"{gate}_{layer}"
+    dimred_key = f"diffmap_{uns_key}"
+    neighbors_key = f"{uns_key}_neighbors"
+    connectivities_key = f"{neighbors_key}_connectivities"
+    
+    preprocessed_adata = _preprocess_adata(adata = adata,
+                                           gate = gate,
+                                           layer = layer,
+                                           use_only_fluo = use_only_fluo,
+                                           exclude = exclude,
+                                           scaling = scaling)
+    
+    if f"X_pca_{uns_key}" not in adata.obsm or recalculate_pca:
+        print("computing PCA for diffmap")
+        pca_kwargs = _extract_valid_pca_kwargs(kwargs)
+        adata = _pca(adata = adata,
+                     preprocessed_adata = preprocessed_adata,
+                     dimred_key = dimred_key,
+                     **pca_kwargs)    
+
+    if connectivities_key not in adata.obsp:
+        print("computing neighbors for diffmap")
+        neighbors_kwargs = _extract_valid_neighbors_kwargs(kwargs)
+        adata = _neighbors(adata = adata,
+                           preprocessed_adata = preprocessed_adata,
+                           neighbors_key = neighbors_key,
+                           **neighbors_kwargs)
+
+    adata = _diffmap(adata = adata,
+                     preprocessed_adata = preprocessed_adata,
+                     neighbors_key = neighbors_key,
+                     uns_key = uns_key,
+                     dimred_key = dimred_key)
+    del adata.X
+    return adata if copy else None
+
+def umap(adata: AnnData,
+         gate: str,
+         layer: Literal["compensated", "transformed"] = "transformed",
+         recalculate_pca: bool = False,
+         use_only_fluo: bool = True,
+         exclude: Optional[list[str]] = None,
+         scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
+         copy: bool = False,
+         *args,
+         **kwargs) -> Optional[AnnData]:
+
+    adata = adata.copy() if copy else adata
+
+    exclude = [] if exclude is None else exclude
+
+    if not scaling in IMPLEMENTED_SCALERS and scaling is not None:
+        raise InvalidScalingError(scaler = scaling)
+
+    _save_dr_settings(adata = adata,
+                      gate = gate,
+                      layer = layer,
+                      use_only_fluo = use_only_fluo,
+                      exclude = exclude,
+                      scaling = scaling,
+                      reduction = "pca",
+                      *args,
+                      **kwargs)
+    
+    uns_key = f"{gate}_{layer}"
+    dimred_key = f"umap_{uns_key}"
+    neighbors_key = f"{uns_key}_neighbors"
+    connectivities_key = f"{neighbors_key}_connectivities"
+    
+    preprocessed_adata = _preprocess_adata(adata = adata,
+                                           gate = gate,
+                                           layer = layer,
+                                           use_only_fluo = use_only_fluo,
+                                           exclude = exclude,
+                                           scaling = scaling)
+
+    if kwargs.get("use_rep") is None:
+        if f"X_pca_{uns_key}" not in adata.obsm or recalculate_pca:
+            pca_kwargs = _extract_valid_pca_kwargs(kwargs)
+            adata = _pca(adata = adata,
+                         preprocessed_adata = preprocessed_adata,
+                         dimred_key = dimred_key,
+                         **pca_kwargs)
+
+    if connectivities_key not in adata.obsp:
+        print("computing neighbors for diffmap")
+        neighbors_kwargs = _extract_valid_neighbors_kwargs(kwargs)
+        adata = _neighbors(adata = adata,
+                           preprocessed_adata = preprocessed_adata,
+                           neighbors_key = neighbors_key,
+                           **neighbors_kwargs)
+
+    umap_kwargs = _extract_valid_umap_kwargs(kwargs)
+    adata = _umap(adata = adata,
+                  preprocessed_adata = preprocessed_adata,
+                  neighbors_key = neighbors_key,
+                  dimred_key = dimred_key,
+                  uns_key = uns_key,
+                  **umap_kwargs)
+
+    del adata.X
+
+    return adata if copy else None
+
+def tsne(adata: AnnData,
+         gate: str,
+         layer: Literal["compensated", "transformed"] = "transformed",
+         use_only_fluo: bool = True,
+         exclude: Optional[list[str]] = None,
+         scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
+         recalculate_pca: bool = False,
+         copy: bool = False,
+         *args,
+         **kwargs) -> Optional[AnnData]:
+
+    adata = adata.copy() if copy else adata
+
+    exclude = [] if exclude is None else exclude
+
+    if not scaling in IMPLEMENTED_SCALERS and scaling is not None:
+        raise InvalidScalingError(scaler = scaling)
+    
+    _save_dr_settings(adata = adata,
+                      gate = gate,
+                      layer = layer,
+                      use_only_fluo = use_only_fluo,
+                      exclude = exclude,
+                      scaling = scaling,
+                      reduction = "pca",
+                      *args,
+                      **kwargs)
+
+    preprocessed_adata = _preprocess_adata(adata = adata,
+                                           gate = gate,
+                                           layer = layer,
+                                           use_only_fluo = use_only_fluo,
+                                           exclude = exclude,
+                                           scaling = scaling)
+    
+    uns_key = f"{gate}_{layer}"
+    dimred_key = f"tsne_{uns_key}"
+    
+    if kwargs.get("use_rep") is None:
+        if f"X_pca_{uns_key}" not in adata.obsm or recalculate_pca:
+            pca_kwargs = _extract_valid_pca_kwargs(kwargs)
+            adata = _pca(adata = adata,
+                         preprocessed_adata = preprocessed_adata,
+                         dimred_key = dimred_key,
+                         **pca_kwargs)
+
+    tsne_kwargs = _extract_valid_tsne_kwargs(kwargs)
+    adata = _tsne(adata = adata,
+                  preprocessed_adata = preprocessed_adata,
+                  uns_key = uns_key,
+                  dimred_key = dimred_key,
+                  **tsne_kwargs)
+    del adata.X
+    return adata if copy else None
 
 def _save_dr_settings(adata: AnnData,
                       gate,
@@ -33,243 +270,3 @@ def _save_dr_settings(adata: AnnData,
     return
 
 
-def pca(adata: AnnData,
-        gate: str,
-        layer: Literal["compensated", "transformed"] = "transformed",
-        use_only_fluo: bool = True,
-        exclude: Optional[list[str]] = None,
-        scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
-        copy: bool = False,
-        *args,
-        **kwargs) -> Optional[AnnData]:
-
-    adata = adata.copy() if copy else adata
-
-    _save_dr_settings(adata = adata,
-                      gate = gate,
-                      layer = layer,
-                      use_only_fluo = use_only_fluo,
-                      exclude = exclude,
-                      scaling = scaling,
-                      reduction = "pca",
-                      *args,
-                      **kwargs)
-    
-    preprocessed_adata = _preprocess_adata(adata = adata,
-                                           gate = gate,
-                                           layer = layer,
-                                           use_only_fluo = use_only_fluo,
-                                           exclude = exclude,
-                                           scaling = scaling)
-    
-    uns_key = f"{gate}_{layer}"
-    dimred_key = f"{uns_key}_pca"
-    
-    sc.pp.pca(preprocessed_adata,
-              random_state = 187,
-              *args,
-              **kwargs)
-    
-    adata = merge_pca_info_into_adata(adata,
-                                      preprocessed_adata,
-                                      dimred_key = dimred_key,
-                                      uns_key = uns_key)
-
-    return adata if copy else None
-
-def diffmap(adata: AnnData,
-            gate: str,
-            layer: Literal["compensated", "transformed"] = "transformed",
-            recalculate_pca: bool = False,
-            use_only_fluo: bool = True,
-            exclude: Optional[list[str]] = None,
-            scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
-            copy: bool = False,
-            *args,
-            **kwargs) -> Optional[AnnData]:
-
-    adata = adata.copy() if copy else adata
-
-    _save_dr_settings(adata = adata,
-                      gate = gate,
-                      layer = layer,
-                      use_only_fluo = use_only_fluo,
-                      exclude = exclude,
-                      scaling = scaling,
-                      reduction = "pca",
-                      *args,
-                      **kwargs)
-    
-    
-    uns_key = f"{gate}_{layer}"
-    dimred_key = f"{uns_key}_diffmap"
-    neighbors_key = f"{uns_key}_neighbors"
-    connectivities_key = f"{neighbors_key}_connectivities"
-    
-    preprocessed_adata = _preprocess_adata(adata = adata,
-                                           gate = gate,
-                                           layer = layer,
-                                           use_only_fluo = use_only_fluo,
-                                           exclude = exclude,
-                                           scaling = scaling)
-    
-    if f"X_{uns_key}_pca" not in adata.obsm or recalculate_pca:
-        print("computing PCA for diffmap")
-        sc.pp.pca(preprocessed_adata,
-                  random_state = 187)
-        adata = merge_pca_info_into_adata(adata,
-                                          preprocessed_adata,
-                                          dimred_key = f"{uns_key}_pca",
-                                          uns_key = uns_key)
-
-    if connectivities_key not in adata.obsp:
-        print("computing neighbors for diffmap")
-        sc.pp.neighbors(preprocessed_adata,
-                        random_state = 187,
-                        key_added = neighbors_key)
-        try:
-            adata = merge_neighbors_info_into_adata(adata,
-                                                    preprocessed_adata,
-                                                    neighbors_key = neighbors_key)
-        except MemoryError:
-            print("Neighbors information could not be merged due to a memory error")
-
- 
-    sc.tl.diffmap(preprocessed_adata,
-                  neighbors_key = neighbors_key,
-                  n_comps = 3,
-                  *args,
-                  **kwargs)
-
-    adata = merge_dimred_coordinates_into_adata(adata = adata,
-                                                gate_subset = preprocessed_adata,
-                                                dimred = "diffmap",
-                                                dimred_key = dimred_key)
-    adata = add_uns_data(adata = adata,
-                         gate_subset = preprocessed_adata,
-                         old_key = "diffmap_evals",
-                         key_added = f"{uns_key}_diffmap_evals")
-
-    return adata if copy else None
-
-def umap(adata: AnnData,
-         gate: str,
-         layer: Literal["compensated", "transformed"] = "transformed",
-         recalculate_pca: bool = False,
-         use_only_fluo: bool = True,
-         exclude: Optional[list[str]] = None,
-         scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
-         copy: bool = False,
-         *args,
-         **kwargs) -> Optional[AnnData]:
-
-    adata = adata.copy() if copy else adata
-
-    _save_dr_settings(adata = adata,
-                      gate = gate,
-                      layer = layer,
-                      use_only_fluo = use_only_fluo,
-                      exclude = exclude,
-                      scaling = scaling,
-                      reduction = "pca",
-                      *args,
-                      **kwargs)
-    
-    uns_key = f"{gate}_{layer}"
-    dimred_key = f"{uns_key}_umap"
-    neighbors_key = f"{uns_key}_neighbors"
-    connectivities_key = f"{neighbors_key}_connectivities"
-    
-    preprocessed_adata = _preprocess_adata(adata = adata,
-                                           gate = gate,
-                                           layer = layer,
-                                           use_only_fluo = use_only_fluo,
-                                           exclude = exclude,
-                                           scaling = scaling)
-
-    if f"X_{uns_key}_pca" not in adata.obsm or recalculate_pca:
-        print("computing PCA for umap")
-        sc.pp.pca(preprocessed_adata,
-                  random_state = 187)
-        adata = merge_pca_info_into_adata(adata,
-                                          preprocessed_adata,
-                                          dimred_key = f"{uns_key}_pca",
-                                          uns_key = uns_key)
-
-    if connectivities_key not in adata.obsp:
-        print("computing neighbors for umap")
-        sc.pp.neighbors(preprocessed_adata,
-                        random_state = 187,
-                        key_added = neighbors_key)
-        try:
-            adata = merge_neighbors_info_into_adata(adata,
-                                                    preprocessed_adata,
-                                                    neighbors_key = neighbors_key)
-        except MemoryError:
-            print("Neighbors information could not be merged due to a memory error")
-    
-    sc.tl.umap(preprocessed_adata,
-               neighbors_key = neighbors_key,
-               n_components = 3,
-               *args,
-               **kwargs)
-    
-    adata = merge_dimred_coordinates_into_adata(adata = adata,
-                                                gate_subset = preprocessed_adata,
-                                                dimred = "umap",
-                                                dimred_key = dimred_key)
-    
-    adata = add_uns_data(adata = adata,
-                         gate_subset = preprocessed_adata,
-                         old_key = "umap",
-                         key_added = f"{uns_key}_umap")
-  
-    return adata if copy else None
-
-def tsne(adata: AnnData,
-         gate: str,
-         layer: Literal["compensated", "transformed"] = "transformed",
-         use_only_fluo: bool = True,
-         exclude: Optional[list[str]] = None,
-         scaling: Optional[Literal["MinMaxScaler", "RobustScaler", "StandardScaler"]] = None,
-         copy: bool = False,
-         *args,
-         **kwargs) -> Optional[AnnData]:
-
-    adata = adata.copy() if copy else adata
-
-    _save_dr_settings(adata = adata,
-                      gate = gate,
-                      layer = layer,
-                      use_only_fluo = use_only_fluo,
-                      exclude = exclude,
-                      scaling = scaling,
-                      reduction = "pca",
-                      *args,
-                      **kwargs)
-    
-    preprocessed_adata = _preprocess_adata(adata = adata,
-                                           gate = gate,
-                                           layer = layer,
-                                           use_only_fluo = use_only_fluo,
-                                           exclude = exclude,
-                                           scaling = scaling)
-    
-    uns_key = f"{gate}_{layer}"
-    dimred_key = f"{uns_key}_tsne"
-
-    sc.tl.tsne(preprocessed_adata,
-               *args,
-               **kwargs)
-
-    adata = merge_dimred_coordinates_into_adata(adata = adata,
-                                                gate_subset = preprocessed_adata,
-                                                dimred = "tsne",
-                                                dimred_key = dimred_key)
-    
-    adata = add_uns_data(adata = adata,
-                         gate_subset = preprocessed_adata,
-                         old_key = "tsne",
-                         key_added = f"{uns_key}_tsne")
-
-    return adata if copy else None
