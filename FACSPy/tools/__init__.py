77d6a057343db898c81f7bc69e076513b1e16ada
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-21 13:09:51 +0100

    added aliases

--- FACSPy/tools/__init__.py
+++ FACSPy/tools/__init__.py
@@ -1,6 +1,4 @@
 from ._deg import *
-from ._dr_samplewise import pca_samplewise, mds_samplewise, tsne_samplewise, umap_samplewise
-from ._fold_change import calculate_fold_changes ## probably never needed?
 from ._fop import fop
 from ._mfi import mfi
 from ._gate_freq import gate_frequencies
@@ -10,3 +8,8 @@ from ._parc import parc
 from ._flowsom import flowsom
 from ._leiden import leiden
 from ._phenograph import phenograph
+from ._neighbors import neighbors
+from ._pca import pca_samplewise
+from ._tsne import tsne_samplewise
+from ._mds import mds_samplewise
+from ._umap import umap_samplewise
