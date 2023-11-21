36716c89fd376f662c7eb00e658594c2591b9288
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-21 13:12:16 +0100

    refactored to own module

---
+++ FACSPy/tools/_mds.py
@@ -0,0 +1,34 @@
+from anndata import AnnData
+
+from typing import Literal, Union, Optional
+
+from ._dr_samplewise import _perform_samplewise_dr
+
+def mds_samplewise(adata: AnnData,
+                   data_group: Optional[Union[str, list[str]]] = "sample_ID",
+                   data_metric: Literal["mfi", "fop", "gate_frequency"] = "mfi",
+                   layer: Literal["compensated", "transformed"] = "compensated",
+                   use_only_fluo: bool = True,
+                   exclude: Optional[Union[str, list, str]] = None,
+                   scaling: Literal["MinMaxScaler", "RobustScaler", "StandardScaler"] = "MinMaxScaler",
+                   n_components: int = 3,
+                   copy = False,
+                   *args,
+                   **kwargs) -> Optional[AnnData]:
+
+    adata = adata.copy() if copy else adata
+
+    adata = _perform_samplewise_dr(adata = adata,
+                                   reduction = "MDS",
+                                   data_metric = data_metric,
+                                   data_group = data_group,
+                                   layer = layer,
+                                   use_only_fluo = use_only_fluo,
+                                   exclude = exclude,
+                                   scaling = scaling,
+                                   n_components = n_components,
+                                   *args,
+                                   **kwargs)
+
+    return adata if copy else None
+
