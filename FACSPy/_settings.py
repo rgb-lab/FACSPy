3a0368bbecd5ba1d0b813f2ed788397786cf83e6
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-24 12:43:41 +0100

    added settings class

---
+++ FACSPy/_settings.py
@@ -0,0 +1,47 @@
+
+from typing import Union
+
+class FACSPyConfig:
+    """
+    Config Manager, inspired by scanpy
+    """
+
+    def __init__(self,
+                 *,
+                 n_jobs = 1,
+                 layer = "compensated",
+                 gate = ""
+                 ):
+        self.n_jobs = n_jobs
+        self.default_layer = layer
+        self.default_gate = gate
+
+    @property
+    def n_jobs(self):
+        return self._n_jobs
+
+    @n_jobs.setter
+    def n_jobs(self,
+               n_jobs: Union[int, float]):
+        self._n_jobs = n_jobs
+
+    @property
+    def default_layer(self):
+        return self._default_layer
+    
+    @default_layer.setter
+    def default_layer(self,
+                      layer):
+        self._default_layer = layer
+
+    @property
+    def default_gate(self):
+        return self._default_gate
+
+    @default_gate.setter
+    def default_gate(self,
+                     gate: str):
+        self._default_gate = gate
+
+
+settings = FACSPyConfig()
