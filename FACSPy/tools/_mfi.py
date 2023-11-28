109d13fd3a0bd1082f736775f4b96ab9b3221dc2
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-28 10:40:58 +0100

    rewritten and aggregate kwarg added

--- FACSPy/tools/_mfi.py
+++ FACSPy/tools/_mfi.py
@@ -17,9 +17,14 @@ def _calculate_metric_from_frame(input_frame: pd.DataFrame,
                                  gate,
                                  fluo_columns,
                                  groupby: Optional[str],
-                                 method: Literal["mean", "median"]) -> pd.DataFrame:
-    groups = ["sample_ID", groupby] if groupby != "sample_ID" else ["sample_ID"]
-    data = input_frame.loc[input_frame[gate] == True, fluo_columns + groups].groupby(groups, observed = True)
+                                 method: Literal["mean", "median"],
+                                 aggregate: bool) -> pd.DataFrame:
+    if aggregate:
+        groups = [groupby]
+    else:
+        groups = ["sample_ID", groupby] if groupby != "sample_ID" else [groupby]
+    data = input_frame.loc[input_frame[gate] == True,
+                           fluo_columns + groups].groupby(groups, observed = True)
     if method == "mean":
         data = _mean(data)
     if method == "median":
@@ -27,44 +32,6 @@ def _calculate_metric_from_frame(input_frame: pd.DataFrame,
     data["gate"] = gate
     data = data.set_index(["gate"], append = True)
     return data.dropna(axis = 0, how = "all")
-
-@_default_layer
-def mfi(adata: AnnData,
-        groupby: Union[Literal["sample_ID"], str] = "sample_ID",
-        layer: Union[str, list[str]] = None,
-        method: Literal["mean", "median"] = "median",
-        use_only_fluo: bool = False,
-        copy: bool = False) -> Optional[AnnData]:
-    adata = adata.copy() if copy else adata
-
-    if not isinstance(layer, list):
-        layer = [layer]
-    
-    if method not in ["median", "mean"]:
-        raise NotImplementedError("metric must be one of ['median', 'mean', 'geo_mean']")
-
-    if use_only_fluo:
-        columns_to_analyze = _fetch_fluo_channels(adata)
-    else:
-        columns_to_analyze = adata.var_names.tolist()
-
-    for _layer in layer:
-        dataframe = _concat_gate_info_and_obs_and_fluo_data(adata,
-                                                            layer = _layer)
-        
-        adata.uns[f"mfi_{'_'.join([groupby])}_{_layer}"] = pd.concat([_calculate_metric_from_frame(dataframe,
-                                                                                                   gate,
-                                                                                                   columns_to_analyze,
-                                                                                                   groupby,
-                                                                                                   method)
-                                                                      for gate in adata.uns["gating_cols"]])
-        _save_settings(adata = adata,
-                       groupby = groupby,
-                       method = method,
-                       use_only_fluo = use_only_fluo,
-                       layer = _layer)
-
-    return adata if copy else None
 
 def _save_settings(adata: AnnData,
                    groupby: str,
@@ -83,3 +50,72 @@ def _save_settings(adata: AnnData,
     }
 
     return 
+
+def _mfi(adata: AnnData,
+         layer: str,
+         columns_to_analyze: list[str],
+         groupby: Union[str, list[str]],
+         method: Literal["mean", "median", "geo_mean"],
+         aggregate: bool) -> pd.DataFrame:
+
+    dataframe = _concat_gate_info_and_obs_and_fluo_data(adata,
+                                                        layer = layer)
+    mfi_frame = pd.concat([_calculate_metric_from_frame(dataframe,
+                                                        gate,
+                                                        columns_to_analyze,
+                                                        groupby,
+                                                        method,
+                                                        aggregate)
+                            for gate in adata.uns["gating_cols"]])
+    return mfi_frame
+
+@_default_layer
+def mfi(adata: AnnData,
+        groupby: Union[Literal["sample_ID"], str] = "sample_ID",
+        layer: Union[str, list[str]] = None,
+        method: Literal["mean", "median"] = "median",
+        use_only_fluo: bool = False,
+        aggregate: bool = False,
+        copy: bool = False) -> Optional[AnnData]:
+    adata = adata.copy() if copy else adata
+
+    if not isinstance(layer, list):
+        layer = [layer]
+    
+    if method not in ["median", "mean"]:
+        raise NotImplementedError("metric must be one of ['median', 'mean', 'geo_mean']")
+
+    if use_only_fluo:
+        columns_to_analyze = _fetch_fluo_channels(adata)
+    else:
+        columns_to_analyze = adata.var_names.tolist()
+
+    for _layer in layer:
+        mfi_frame = _mfi(adata = adata,
+                         layer = _layer,
+                         columns_to_analyze = columns_to_analyze,
+                         groupby = groupby,
+                         method = method,
+                         aggregate = aggregate)
+
+        adata.uns[f"mfi_{'_'.join([groupby])}_{_layer}"] = mfi_frame
+
+        _save_settings(adata = adata,
+                       groupby = groupby,
+                       method = method,
+                       use_only_fluo = use_only_fluo,
+                       layer = _layer)
+
+    return adata if copy else None
+
+ 
+# old function...
+#        dataframe = _concat_gate_info_and_obs_and_fluo_data(adata,
+#                                                            layer = _layer)
+#        
+#        adata.uns[f"mfi_{'_'.join([groupby])}_{_layer}"] = pd.concat([_calculate_metric_from_frame(dataframe,
+#                                                                                                   gate,
+#                                                                                                   columns_to_analyze,
+#                                                                                                   groupby,
+#                                                                                                   method)
+#                                                                      for gate in adata.uns["gating_cols"]])
