873045728ad37ad2159c7b027b19374868b831a2
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-28 11:31:08 +0100

    added NANRemovalWarning

--- FACSPy/exceptions/_exceptions.py
+++ FACSPy/exceptions/_exceptions.py
@@ -1,6 +1,14 @@
 from typing import Any
 import warnings
 
+class NaNRemovalWarning(Warning):
+    def __init__(self,
+                 message) -> None:
+        self.message = message
+    
+    def __str__(self):
+        return repr(self.message)
+
 class InvalidTransformationError(Exception):
 
     def __init__(self,
