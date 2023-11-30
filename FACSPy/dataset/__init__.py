6642809d09cfa789589e9fc987036844abf45d2a
TarikExner <Tarik.Exnerq@gmail.com>
2023-11-30 11:29:10 +0100

    added aliases for utils create_empty_metadata and _create_panel_from_fcs

--- FACSPy/dataset/__init__.py
+++ FACSPy/dataset/__init__.py
@@ -2,3 +2,4 @@ from ._dataset import DatasetAssembler, Transformer, create_dataset
 from ._transformation import transform, calculate_cofactors
 from ._supplements import CofactorTable, Metadata, Panel
 from ._workspaces import FlowJoWorkspace, DivaWorkspace
+from ._utils import create_empty_metadata, create_panel_from_fcs
