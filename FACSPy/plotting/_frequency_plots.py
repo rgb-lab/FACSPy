d3bdac1e0581b0d3f02dca801a9c64488ee05675
TarikExner <Tarik.Exnerq@gmail.com>
2023-12-06 20:35:40 +0100

    added show == False return ax statement and logic, documentation

--- FACSPy/plotting/_frequency_plots.py
+++ FACSPy/plotting/_frequency_plots.py
@@ -3,6 +3,7 @@ import pandas as pd
 from typing import Union
 
 from matplotlib.figure import Figure
+from matplotlib.axes import Axes
 
 from matplotlib import pyplot as plt
 
@@ -33,13 +34,54 @@ def _order_dataframe(dataframe: pd.DataFrame,
 
 def cluster_frequency(adata: AnnData,
                       groupby: Union[str, list[str]],
-                      order: Optional[list[str]] = None,
-                      cluster_key: str = "leiden",
+                      cluster_key: str = None,
                       normalize: bool = True,
+                      order: Optional[list[str]] = None,
+                      figsize: tuple[float, float] = (5,4),
+                      ax: Axes = None,
                       return_dataframe: bool = False,
                       return_fig: bool = False,
                       save: bool = None,
                       show: bool = None) -> Optional[Figure]:
+    """
+    Plots the frequency as a stacked bar chart of a grouping variable per cluster.
+
+    Parameters
+    ----------
+
+    adata
+        The anndata object of shape `n_obs` x `n_vars`
+        where rows correspond to cells and columns to the channels
+    groupby
+        controls the x axis and the grouping of the data points
+    cluster_key
+        The obs slot where the clusters of interest are stored
+    normalize
+        Whether to normalize the frequencies to 1.
+    order
+        Sets the order of the groupby variable.
+    figsize
+        Contains the dimensions of the final figure as a tuple of two ints or floats
+    save
+        Expects a file path and a file name. saves the figure to the indicated path
+    show
+        Whether to show the figure
+    return_dataframe
+        If set to True, returns the raw data that are used for plotting. vmin and vmax
+        are not set.
+    return_fig
+        If set to True, the figure is returned.
+    ax
+        Optional parameter. Sets user defined ax from for example plt.subplots
+
+    Returns
+    -------
+
+    if `show==False` a :class:`~seaborn.ClusterGrid`
+
+    """
+
+    
     
     dataframe = _prep_dataframe_cluster_freq(adata, groupby, cluster_key, normalize)
     
@@ -49,7 +91,9 @@ def cluster_frequency(adata: AnnData,
     if return_dataframe:
         return dataframe
     
-    fig, ax = plt.subplots(ncols = 1, nrows = 1, figsize = (5,4))
+    if ax is None:
+        fig = plt.figure(figsize = figsize)
+        ax = fig.add_subplot(111)
     dataframe.plot(kind = "bar",
                    stacked = True,
                    ax = ax)
@@ -61,6 +105,8 @@ def cluster_frequency(adata: AnnData,
 
     if return_fig:
         return fig
-    plt.tight_layout()
 
     savefig_or_show(save = save, show = show)
+
+    if show is False:
+        return ax
