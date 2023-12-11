cff1e608683b1fd8d7e2a588f077f5f6a6cb7562
TarikExner <Tarik.Exnerq@gmail.com>
2023-12-11 18:28:18 +0100

    removed clustering due to error in phenograph

--- FACSPy/tools/_phenograph.py
+++ FACSPy/tools/_phenograph.py
@@ -56,30 +56,30 @@ def phenograph(adata: AnnData,
                                            exclude = exclude,
                                            scaling = scaling)
 
-    if f"X_pca_{uns_key}" not in adata.obsm:
-        print("computing PCA for parc!")
-        pca_kwargs = _extract_valid_pca_kwargs(kwargs)
-        adata = _pca(adata = adata,
-                     preprocessed_adata = preprocessed_adata,
-                     dimred_key = f"pca_{uns_key}",
-                     **pca_kwargs)
-        preprocessed_adata = _recreate_preprocessed_view(adata,
-                                                         preprocessed_adata)
-
-    if connectivities_key not in adata.obsp:
-        print("computing neighbors for parc!")
-        neighbors_kwargs = _extract_valid_neighbors_kwargs(kwargs)
-        if not "use_rep" in neighbors_kwargs:
-            neighbors_kwargs["use_rep"] = _choose_use_rep_as_scanpy(adata,
-                                                                    uns_key = uns_key,
-                                                                    use_rep = None,
-                                                                    n_pcs = neighbors_kwargs.get("n_pcs"))
-        adata = _neighbors(adata = adata,
-                           preprocessed_adata = preprocessed_adata,
-                           neighbors_key = neighbors_key,
-                           **neighbors_kwargs)
-        preprocessed_adata = _recreate_preprocessed_view(adata,
-                                                         preprocessed_adata)
+#    if f"X_pca_{uns_key}" not in adata.obsm:
+#        print("computing PCA for phenograph!")
+#        pca_kwargs = _extract_valid_pca_kwargs(kwargs)
+#        adata = _pca(adata = adata,
+#                     preprocessed_adata = preprocessed_adata,
+#                     dimred_key = f"pca_{uns_key}",
+#                     **pca_kwargs)
+#        preprocessed_adata = _recreate_preprocessed_view(adata,
+#                                                         preprocessed_adata)
+#
+#    if connectivities_key not in adata.obsp:
+#        print("computing neighbors for phenograph!")
+#        neighbors_kwargs = _extract_valid_neighbors_kwargs(kwargs)
+#        if not "use_rep" in neighbors_kwargs:
+#            neighbors_kwargs["use_rep"] = _choose_use_rep_as_scanpy(adata,
+#                                                                    uns_key = uns_key,
+#                                                                    use_rep = None,
+#                                                                    n_pcs = neighbors_kwargs.get("n_pcs"))
+#        adata = _neighbors(adata = adata,
+#                           preprocessed_adata = preprocessed_adata,
+#                           neighbors_key = neighbors_key,
+#                           **neighbors_kwargs)
+#        preprocessed_adata = _recreate_preprocessed_view(adata,
+#                                                         preprocessed_adata)
 
 
     preprocessed_adata = _preprocess_adata(adata,
@@ -91,8 +91,7 @@ def phenograph(adata: AnnData,
     if not "k" in kwargs:
         kwargs["k"] = min(preprocessed_adata.shape[0] - 1, 30)
 
-    communities, graph, Q = _phenograph.cluster(preprocessed_adata.obsp[connectivities_key]
-                                                if connectivities_key in adata.obsp else preprocessed_adata.X,
+    communities, graph, Q = _phenograph.cluster(preprocessed_adata.X,
                                                 **kwargs)
 
     adata = _merge_cluster_info_into_adata(adata,
