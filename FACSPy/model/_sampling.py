44d3435512cb59c32314fdcda0dcf4008bc54930
TarikExner <Tarik.Exnerq@gmail.com>
2024-01-29 19:46:56 +0100

    bugfixes, rewrites etc

--- FACSPy/model/_sampling.py
+++ FACSPy/model/_sampling.py
@@ -110,7 +110,7 @@ class GateSampler:
                                         undersampler)
         
         self.gating = self._create_gating_frame(adata)
-        self.CELL_THRESHOLD = gate_freq_cutoff or 10
+        self.CELL_THRESHOLD = gate_freq_cutoff or 50
         self.target_size = target_size_per_gate
         self.target_size_half = int(np.ceil(self.target_size/2))
 
@@ -166,7 +166,14 @@ class GateSampler:
         return classes_above_threshold_idxs, classes_below_threshold_idxs
 
     def _map_gate_matrix(self,
-                         y: np.ndarray) -> tuple[np.ndarray, dict]:
+                         y: np.ndarray) -> tuple[dict, dict]:
+        """
+        Function maps binary integers to ascending integers.
+        We need that in case we have more than 64 gates, because
+        the binarized gating strategy will contain two 64bit integers
+        """
+        if y.shape[1] == 1:
+            y = np.squeeze(y)
         uniques = np.unique(y, axis = 0)
         if y.ndim == 1:
             return (
@@ -182,10 +189,16 @@ class GateSampler:
     def _apply_gate_map(self,
                         y: np.ndarray,
                         gate_map: dict) -> np.ndarray:
+        if y.shape[1] == 1:
+            y = np.squeeze(y)
         if y.ndim == 1:
-            return [gate_map.__getitem__(y[i]) for i in range(len(y))]
+            return np.array([gate_map.__getitem__(y[i])
+                             for i in range(len(y))
+                             if y[i] in gate_map.keys()])
         tuple_list = list(map(tuple, y))
-        return np.array(np.array([gate_map.__getitem__(tuple_list[i]) for i in range(len(tuple_list))]))
+        return np.array(np.array([gate_map.__getitem__(tuple_list[i])
+                                  for i in range(len(tuple_list))
+                                  if tuple_list[i] in gate_map.keys()]))
 
     def _calculate_sampling_strategy(self,
                                      y: np.ndarray):
@@ -324,26 +337,35 @@ class GateSampler:
         y_binary = self._convert_gate_matrix_to_binary(y)
         user_defined_gates = self._identify_gates_binaries(self.gating)
 
-        binary_classes, binary_classes_frequency = self._calculate_gate_frequencies(y_binary)
+
+        gate_map, reverse_gate_map = self._map_gate_matrix(y_binary)
+
+        y_mapped = self._apply_gate_map(y_binary, gate_map)
+        user_defined_gates_mapped = self._apply_gate_map(user_defined_gates, gate_map)
+        
+        y_mapped = y_mapped.reshape(y_mapped.shape[0], 1)
+        
+        binary_classes, binary_classes_frequency = self._calculate_gate_frequencies(y_mapped)
         binary_classes = [bin_class[0] for bin_class in binary_classes]
 
         X_sampled = np.empty(shape = (0, X.shape[1]), dtype = np.float64)
-        y_binary_sampled= np.empty(shape = (0, y_binary.shape[1]), dtype = np.int64)
+        y_mapped_sampled = np.empty(shape = (0, y_mapped.shape[1]), dtype = np.int64)
 
         above_thresholds = {
-            bin_class: self.target_size if bin_class in user_defined_gates else count + 1
+            bin_class: self.target_size if bin_class in user_defined_gates_mapped else count + 1
             for bin_class, count
             in zip(binary_classes, binary_classes_frequency)
             if count > self.target_size
         }
         below_thresholds = {
-            bin_class: self.target_size if bin_class in user_defined_gates else count
+            bin_class: self.target_size if bin_class in user_defined_gates_mapped else count
             for bin_class, count
             in zip(binary_classes, binary_classes_frequency)
             if self.CELL_THRESHOLD < count <= self.target_size
         }
+        print(below_thresholds)
         below_cutoff = {
-            bin_class: self.target_size if bin_class in user_defined_gates else count
+            bin_class: self.target_size if bin_class in user_defined_gates_mapped else count
             for bin_class, count
             in zip(binary_classes, binary_classes_frequency)
             if count <= self.CELL_THRESHOLD
@@ -351,26 +373,28 @@ class GateSampler:
         }
 
         for bin_class in above_thresholds:
-            idxs = np.where(y_binary == bin_class)[0]
-            X_, y_ = self._random_subsample(X[idxs], y_binary[idxs], self.target_size)
+            idxs = np.where(y_mapped == bin_class)[0]
+            X_, y_ = self._random_subsample(X[idxs], y_mapped[idxs], self.target_size)
             X_sampled = np.vstack([X_sampled, X_])
-            y_binary_sampled = np.vstack([y_binary_sampled, y_])
+            y_mapped_sampled = np.vstack([y_mapped_sampled, y_])
         
-        idxs = np.where(np.isin(y_binary, list(below_thresholds.keys())))[0]
+        idxs = np.where(np.isin(y_mapped, list(below_thresholds.keys())))[0]
         oversampler: BaseOverSampler = self.oversampler(sampling_strategy = below_thresholds)
-        X_, y_ = oversampler.fit_resample(X[idxs], y_binary[idxs])
+        X_, y_ = oversampler.fit_resample(X[idxs], y_mapped[idxs])
         y_ = y_.reshape(y_.shape[0], 1)
         X_sampled = np.vstack([X_sampled, X_])
-        y_binary_sampled = np.vstack([y_binary_sampled, y_])
+        y_mapped_sampled = np.vstack([y_mapped_sampled, y_])
 
-        idxs = np.where(np.isin(y_binary, list(below_cutoff.keys())))[0]
-        oversampler: BaseOverSampler = RandomOverSampler(sampling_strategy = below_cutoff)
-        X_, y_ = oversampler.fit_resample(X[idxs], y_binary[idxs])
-        y_ = y_.reshape(y_.shape[0], 1)
-        X_sampled = np.vstack([X_sampled, X_])
-        y_binary_sampled = np.vstack([y_binary_sampled, y_])
+        #idxs = np.where(np.isin(y_mapped, list(below_cutoff.keys())))[0]
+        #oversampler: BaseOverSampler = RandomOverSampler(sampling_strategy = below_cutoff)
+        #X_, y_ = oversampler.fit_resample(X[idxs], y_mapped[idxs])
+        #y_ = y_.reshape(y_.shape[0], 1)
+        #X_sampled = np.vstack([X_sampled, X_])
+        #y_mapped_sampled = np.vstack([y_mapped_sampled, y_])
 
-        y_sampled = self._convert_binary_to_gate_matrix(y_binary_sampled.astype(np.int64))
+        y_binary_sampled = self._apply_gate_map(y_mapped_sampled, reverse_gate_map)
+
+        y_sampled = self._convert_binary_to_gate_matrix(y_binary_sampled.astype(np.int64).reshape(y_binary_sampled.shape[0], y_binary.shape[1]))
 
         return self._shuffle(X_sampled, y_sampled) if shuffle else (X_sampled, y_sampled)
 
@@ -477,16 +501,13 @@ class GateSampler:
                 [0 if not gate in parents else 1
                  for gate in gating.columns]
             )
-            gate_coding = gate_coding.reshape(1, gate_coding.shape[0])
             gates_to_keep.append(
-                int(
-                    self._convert_gate_matrix_to_binary(
-                        gate_coding
-                    )[0][0]
-                )
+                self._convert_gate_matrix_to_binary(
+                    gate_coding.reshape(1, gate_coding.shape[0])
+                )[0]
             )
         
-        return gates_to_keep
+        return np.array(gates_to_keep, dtype = np.int64)
 
 
     def _fit_resample_above_threshold(self,
@@ -532,12 +553,14 @@ class GateSampler:
         ### the bit conversion is limited to 64bit numbers
         y_list = np.array_split(y, np.ceil(y.shape[1]/64), axis = 1)
         self._bit_ranges = [y_subset.shape[1] for y_subset in y_list]
-        return np.vstack([self.__bits_to_int(y_subset) for y_subset in y_list]).T
+        return np.vstack([self.__bits_to_int(y_subset) for y_subset in y_list]).astype(np.int64).T
 
     def _convert_binary_to_gate_matrix(self,
                                        y: np.ndarray) -> np.ndarray:
+        if y.shape[1] == 1:
+            y = np.squeeze(y)
         if y.ndim == 1:
-            return self.__int_to_bits(y)[:,1:]
+            return self.__int_to_bits(y, self._bit_ranges[0])[:,1:]
         gate_list = [self.__int_to_bits(y[:,i], self._bit_ranges[i]) for i in range(y.shape[1])]
         return np.hstack(gate_list)[:,1:]
 
