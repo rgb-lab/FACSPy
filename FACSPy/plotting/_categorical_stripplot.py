3e0396ee61d96263c9bd50e4461dfd45e0cdfe26
TarikExner <Tarik.Exnerq@gmail.com>
2024-02-02 09:40:44 +0100

    added statistical test draw method

--- FACSPy/plotting/_categorical_stripplot.py
+++ FACSPy/plotting/_categorical_stripplot.py
@@ -2,14 +2,30 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from matplotlib.axes import Axes
 import seaborn as sns
+from statannotations.Annotator import Annotator
+
 from typing import Optional
 
-from ._baseplot import barplot
-from ._basestats import add_statistic
 from ._utils import (CATEGORICAL_BOXPLOT_PARAMS,
                      CATEGORICAL_STRIPPLOT_PARAMS)
+from .._utils import _create_comparisons
 
+def _add_statistic(ax: Axes,
+                   test: str,
+                   dataframe: pd.DataFrame,
+                   groupby: str,
+                   plot_params: dict,
+                   splitby: Optional[str] = None) -> Axes:
+    
+    pairs = _create_comparisons(dataframe, groupby, splitby)
+    annotator = Annotator(ax,
+                          pairs,
+                          **plot_params,
+                          verbose = False)
+    annotator.configure(test = test, text_format = "star", loc = "inside")
+    annotator.apply_and_annotate()
 
+    return ax
 def _categorical_strip_box_plot(ax: Optional[Axes],
                                 data: pd.DataFrame,
                                 plot_params: dict,
@@ -27,8 +43,7 @@ def _categorical_strip_box_plot(ax: Optional[Axes],
     if groupby == "sample_ID":
         if plot_params["hue"]:
             raise TypeError("You selected a splitby parameter while plotting sample ID. Don't.")
-        ax = barplot(ax,
-                     plot_params = plot_params)
+        sns.barplot(**plot_params)
 
     else:
         sns.stripplot(**plot_params,
@@ -39,12 +54,12 @@ def _categorical_strip_box_plot(ax: Optional[Axes],
 
         if stat_test:
             try:
-                ax = add_statistic(ax = ax,
-                                   test = stat_test,
-                                   dataframe = data,
-                                   groupby = groupby,
-                                   splitby = splitby,
-                                   plot_params = plot_params)
+                ax = _add_statistic(ax = ax,
+                                    test = stat_test,
+                                    dataframe = data,
+                                    groupby = groupby,
+                                    splitby = splitby,
+                                    plot_params = plot_params)
             except ValueError as e:
                 if str(e) != "All numbers are identical in kruskal":
                     raise ValueError from e
