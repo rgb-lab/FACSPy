29b5f854aa5e27706508ca89f202fe47579947c7
TarikExner <Tarik.Exnerq@gmail.com>
2024-02-02 09:04:15 +0100

    rewrite for cleaner code

--- FACSPy/plotting/_qc.py
+++ FACSPy/plotting/_qc.py
@@ -5,82 +5,90 @@ from typing import Union, Optional, Literal
 
 import pandas as pd
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
 
 from ..exceptions._exceptions import HierarchyError
-
-from ._utils import savefig_or_show
+from ._categorical_stripplot import _categorical_strip_box_plot
+from ._utils import (savefig_or_show,
+                     CATEGORICAL_BOXPLOT_PARAMS,
+                     CATEGORICAL_STRIPPLOT_PARAMS)
 from ._baseplot import (stripboxplot,
                         barplot,
                         label_plot_basic,
                         adjust_legend)
 from ._basestats import add_statistic
 
-from .._utils import (GATE_SEPARATOR,
-                      find_gate_path_of_gate,
-                      find_parent_gate,
-                      find_grandparent_gate,
-                      subset_gate,
-                      find_parent_population,
-                      find_grandparent_population,
-                      find_current_population,
+from .._utils import (subset_gate,
                       convert_gate_to_obs,
-                      _default_gate)
+                      _find_gate_path_of_gate,
+                      _find_parent_gate,
+                      _find_grandparent_gate,
+                      _find_parent_population,
+                      _find_grandparent_population,
+                      _find_current_population,
+                      _default_gate,
+                      GATE_SEPARATOR)
 from ..exceptions._exceptions import AnalysisNotPerformedError
+from .._settings import settings
 
 def _find_y_label(adata: AnnData,
                   freq_of: Optional[Union[str, list[str], Literal["parent", "grandparent"]]],
                   gate: Union[str, list[str]]):
     
     if freq_of == "parent":
-        return find_parent_population(find_gate_path_of_gate(adata, gate))
+        return _find_parent_population(_find_gate_path_of_gate(adata, gate))
     if freq_of == "grandparent":
-        return find_grandparent_population(find_gate_path_of_gate(adata, gate))
+        return _find_grandparent_population(_find_gate_path_of_gate(adata, gate))
     return "All Cells" if freq_of in ["root", "all"] else freq_of
 
 def _prepare_dataframe_gate_frequency(adata: AnnData,
                                       gate: Union[str, list[str]],
                                       freq_of: Optional[Union[str, list[str]]],
                                       groupby: Optional[Union[str, list[str]]],
-                                      colorby: Optional[str]) -> pd.DataFrame:
+                                      splitby: Optional[str]) -> pd.DataFrame:
     
     if "gate_frequencies" not in adata.uns:
         raise AnalysisNotPerformedError("fp.tl.gate_frequencies")
     df = adata.uns["gate_frequencies"].reset_index()
     
-    if GATE_SEPARATOR not in gate:
-        gate = find_gate_path_of_gate(adata, gate)
+    gate = _find_gate_path_of_gate(adata, gate)
 
     if freq_of not in gate.split(GATE_SEPARATOR)[:-1] and freq_of not in [None, "parent", "grandparent", "all"]:
         raise HierarchyError
     
     if freq_of == "parent":
-        freq_of = find_parent_gate(gate)
+        freq_of = _find_parent_gate(gate)
     elif freq_of is None or freq_of == "all":
         freq_of = "root"
     elif freq_of == "grandparent":
-        freq_of = find_grandparent_gate(gate)
+        freq_of = _find_grandparent_gate(gate)
     elif GATE_SEPARATOR not in freq_of:
-        freq_of = find_gate_path_of_gate(adata, freq_of)
+        freq_of = _find_gate_path_of_gate(adata, freq_of)
 
     df = df.loc[(df["gate"] == gate) & (df["freq_of"] == freq_of)]
     
-    obs = _prepare_dataframe_cell_counts(adata, groupby, colorby = colorby)
+    obs = _prepare_dataframe_cell_counts(adata, groupby, splitby = splitby)
     obs = obs.drop("counts", axis = 1)
     return obs.merge(df, on = "sample_ID")
 
 def _prepare_dataframe_cell_counts(adata: AnnData,
                                    groupby: Optional[str],
-                                   colorby: Optional[str]):
-    import copy
-    groupings = ["sample_ID"] + copy.copy(groupby) if "sample_ID" not in groupby else copy.copy(groupby)
-    if colorby != [None]:
-        groupings += colorby
-    groupings = list(set(groupings)) ## in case the user chooses groupby and colorby as the same
-    if groupby == [None]:
-        return adata.obs["sample_ID"].value_counts().to_frame(name = "counts").reset_index(names = "sample_ID")
+                                   splitby: Optional[str]):
+    if groupby == "sample_ID":
+        groupings = [groupby]
+    elif groupby is None:
+        groupings = ["sample_ID"]
+    else:
+        groupings = ["sample_ID", groupby]
+    #groupings = ["sample_ID"] + copy.copy(groupby) if "sample_ID" not in groupby else copy.copy(groupby)
+    if splitby is not None:
+        groupings.append(splitby)
+    groupings = list(set(groupings)) ## in case the user chooses groupby and splitby as the same
+    #if groupby == None:
+    #    return adata.obs["sample_ID"].value_counts().to_frame(name = "counts").reset_index(names = "sample_ID")
 
     return adata.obs[groupings].value_counts().to_frame(name = "counts").reset_index()
 
@@ -89,9 +97,11 @@ def gate_frequency(adata: AnnData,
                    gate: Union[str, list[str]] = None,
                    freq_of: Optional[Union[str, list[str], Literal["parent", "grandparent", "all"]]] = None,
                    groupby: Optional[str] = None,
-                   colorby: Optional[str] = None,
+                   splitby: Optional[str] = None,
                    cmap: str = None,
-                   figsize: tuple[float, float] = (4,3),
+                   stat_test: str = "Kruskal",
+                   order: list[str] = None,
+                   figsize: tuple[float, float] = (3,3),
                    return_dataframe: bool = False,
                    return_fig: bool = False,
                    ax: Axes = None,
@@ -115,7 +125,7 @@ def gate_frequency(adata: AnnData,
         is displayed
     groupby
         controls the x axis and the grouping of the data points
-    colorby
+    splitby
         controls the coloring of the data points. Defaults to None.
     cmap
         Sets the colormap for plotting. Can be continuous or categorical, depending
@@ -141,64 +151,36 @@ def gate_frequency(adata: AnnData,
     if `show==False` a :class:`~matplotlib.axes.Axes`
     
     """
-    
-    if not isinstance(groupby, list):
-        groupby = [groupby]
 
-    if not isinstance(colorby, list):
-        colorby = [colorby]
-
-    df = _prepare_dataframe_gate_frequency(adata,
-                                           gate,
-                                           freq_of,
-                                           groupby,
-                                           colorby)
+    data = _prepare_dataframe_gate_frequency(adata,
+                                             gate,
+                                             freq_of,
+                                             groupby,
+                                             splitby)
 
     if return_dataframe:
-        return df
+        return data
 
     plot_params = {
-        "x": groupby[0],
+        "data": data,
+        "x": groupby,
         "y": "freq",
-        "hue": colorby[0],
-        "palette": cmap or "Set1",
-        "data": df
+        "hue": splitby,
+        "palette": cmap or settings.default_categorical_cmap if splitby else None,
+        "order": order
     }
 
-    if ax is None:
-        fig = plt.figure(figsize = figsize)
-        ax = fig.add_subplot(111)
+    fig, ax = _categorical_strip_box_plot(ax = ax,
+                                          data = data,
+                                          plot_params = plot_params,
+                                          groupby = groupby,
+                                          splitby = splitby,
+                                          stat_test = stat_test,
+                                          figsize = figsize)
 
-    if groupby == ["sample_ID"]:
-        ax = barplot(ax,
-                     plot_params = plot_params)
-
-    else:
-        ax = stripboxplot(ax,
-                          plot_params = plot_params)
-        try:
-            ax = add_statistic(ax = ax,
-                                test = "Kruskal",
-                                dataframe = df,
-                                groupby = groupby[0],
-                                plot_params = plot_params)
-        except ValueError as e:
-            if str(e) != "All numbers are identical in kruskal":
-                raise ValueError from e
-            else:
-                print("warning... Values were uniform, no statistics to plot.")
-
-    ax = label_plot_basic(ax = ax,
-                          title = f"gate frequency per {groupby[0]}\ngate: {find_current_population(gate)}",
-                          y_label = f"freq. of\n{_find_y_label(adata, freq_of, gate)}",
-                          x_label = "")
-
-    if colorby != [None]:
-        ax = adjust_legend(ax)
-    else:
-        ax.legend().remove()
-    
-    ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = "center")
+    ax.set_title(f"gate frequency per {groupby}\ngate: {_find_current_population(gate)}")
+    ax.set_xlabel("")
+    ax.set_ylabel(f"freq. of\n{_find_y_label(adata, freq_of, gate)}")
 
     if return_fig:
         return fig    
@@ -213,9 +195,11 @@ def gate_frequency(adata: AnnData,
 def cell_counts(adata: AnnData,
                 gate: str = None,
                 groupby: Optional[Union[str, list[str]]] = None,
-                colorby: Optional[str] = None,
+                splitby: Optional[str] = None,
                 cmap: str = None,
-                figsize: tuple[float, float] = (4,3),
+                order: list[str] = None,
+                stat_test: Optional[str] = "Kruskal",
+                figsize: tuple[float, float] = (3,3),
                 return_dataframe: bool = False,
                 return_fig: bool = False,
                 ax: Axes = None,
@@ -271,69 +255,39 @@ def cell_counts(adata: AnnData,
                             copy = False,
                             as_view = True)
 
-    if not isinstance(groupby, list):
-        groupby = [groupby]
-    
-    if not isinstance(colorby, list):
-        colorby = [colorby]
-    
-    df = _prepare_dataframe_cell_counts(adata,
-                                        groupby,
-                                        colorby)
+    data = _prepare_dataframe_cell_counts(adata,
+                                          groupby,
+                                          splitby)
     
     if return_dataframe:
-        return df 
+        return data
     
     plot_params = {
-        "data": df,
-        "x": groupby[0],
+        "data": data,
+        "x": groupby,
         "y": "counts",
-        "hue": colorby[0],
-        "palette": cmap or "Set1"
+        "hue": splitby,
+        "palette": cmap or settings.default_categorical_cmap if splitby else None,
+        "order": order
+
     }
 
-    if ax is None:
-        fig = plt.figure(figsize = figsize)
-        ax = fig.add_subplot(111)
-    
-    if groupby == ["sample_ID"]:
-        ax = barplot(ax,
-                     plot_params = plot_params)
+    fig, ax = _categorical_strip_box_plot(ax = ax,
+                                          data = data,
+                                          plot_params = plot_params,
+                                          groupby = groupby,
+                                          splitby = splitby,
+                                          stat_test = stat_test,
+                                          figsize = figsize)
 
-    else:
-        ax = stripboxplot(ax,
-                          plot_params = plot_params)
-        try:
-            ax = add_statistic(ax = ax,
-                                test = "Kruskal",
-                                dataframe = df,
-                                groupby = groupby[0],
-                                plot_params = plot_params)
-        except ValueError as e:
-            if str(e) != "All numbers are identical in kruskal":
-                raise ValueError from e
-            else:
-                print("warning... Values were uniform, no statistics to plot.")
+    ax.set_title(f"cell counts\ngrouped by {groupby}")
+    ax.set_xlabel("")
+    ax.set_ylabel("cell counts")
 
-    ax = label_plot_basic(ax = ax,
-                          title = f"cell counts\ngrouped by {groupby[0]}",
-                          y_label = "cell counts",
-                          x_label = "")
-    
-    if colorby != [None]:
-        ax = adjust_legend(ax)
-    else:
-        ax.legend().remove()
-
-    
-    ax.set_xticklabels(ax.get_xticklabels(),
-                       rotation = 45,
-                       ha = "center")
-        
     if return_fig:
-        return fig
-    
-    savefig_or_show(save = save, show = show)
+        return fig    
+
+    savefig_or_show(show = show, save = save)
 
     if show is False:
         return ax
