96ed293962185b235c8de07da56622867a7c86e3
TarikExner <Tarik.Exnerq@gmail.com>
2024-03-07 09:21:25 +0100

    added InfRemovalWarning

--- FACSPy/exceptions/_exceptions.py
+++ FACSPy/exceptions/_exceptions.py
@@ -9,6 +9,14 @@ class CofactorNotFoundWarning(Warning):
     def __str__(self):
         return repr(self.message)
 
+class InfRemovalWarning(Warning):
+    def __init__(self,
+                 message) -> None:
+        self.message = message
+
+    def __str__(self):
+        return repr(self.message)
+
 class NaNRemovalWarning(Warning):
     def __init__(self,
                  message) -> None:
