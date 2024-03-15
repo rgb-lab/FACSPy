da5173dcc8f124be316e35707ecc5c56f7bd30c2
Nicolaj Hackert <n-hackert@t-online.de>
2024-03-15 11:35:22 +0100

    Updated tools init file.

--- FACSPy/tools/__init__.py
+++ FACSPy/tools/__init__.py
@@ -1,8 +1,8 @@
-from ._deg import *
+from ._deg import smf
 from ._fop import fop
 from ._mfi import mfi
 from ._gate_freq import gate_frequencies, gate_frequencies_mem
-from ._mst import *
+from ._mst import mst
 from ._dr import umap, tsne, diffmap, pca
 from ._parc import parc
 from ._flowsom import flowsom
@@ -14,3 +14,26 @@ from ._tsne import tsne_samplewise
 from ._mds import mds_samplewise
 from ._umap import umap_samplewise
 from ._correct_expression import correct_expression
+
+__all__ = [
+    "smf",
+    "fop",
+    "mfi",
+    "gate_frequencies",
+    "gate_frequencies_mem",
+    "mst",
+    "umap",
+    "tsne",
+    "diffmap",
+    "pca",
+    "parc",
+    "flowsom",
+    "leiden",
+    "phenograph",
+    "neighbors",
+    "pca_samplewise",
+    "tsne_samplewise",
+    "mds_samplewise",
+    "umap_samplewise",
+    "correct_expression",
+]
