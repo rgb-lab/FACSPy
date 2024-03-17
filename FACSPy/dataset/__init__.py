8e35082f9191936f0444e509aa132893427ba11b
Nicolaj Hackert <n-hackert@t-online.de>
2024-03-17 18:19:11 +0100

    Adde dataset docs.

--- FACSPy/dataset/__init__.py
+++ FACSPy/dataset/__init__.py
@@ -3,3 +3,16 @@ from ._transformation import transform, calculate_cofactors
 from ._supplements import CofactorTable, Metadata, Panel
 from ._workspaces import FlowJoWorkspace, DivaWorkspace
 from ._utils import create_empty_metadata, create_panel_from_fcs
+
+__all__ = [
+    "DatasetAssembler",
+    "Transformer",
+    "create_dataset",
+    "transform",
+    "calculate_cofactors",
+    "CofactorTable",
+    "Metadata",
+    "Panel",
+    "create_empty_metadata",
+    "create_panel_from_fcs",
+]
