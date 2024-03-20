bd4e8c74e8858a3631545bef8ebac0540b0d5530
TarikExner <Tarik.Exnerq@gmail.com>
2024-03-20 07:56:56 +0100

    setting 'simplify_optional_unions' to False to accomodate Optional[Union[...]] rather than Union[..., None]

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@ import sys
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'FACSPy'
-copyright = '2024, Tarik Exner'
-author = 'Tarik Exner'
+copyright = '2024, Tarik Exner, Nicolaj Hackert'
+author = 'Tarik Exner, Nicolaj Hackert'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -34,6 +34,10 @@ bibtex_bibfiles = ['references.bib']
 # Generate the API documentation when building
 autosummary_generate = True
 autodoc_member_order = "bysource"
+
+# autodoc-typehint
+simplify_optional_unions = False
+
 # issues_github_path = ""
 # autodoc_default_flags = ['members']
 napoleon_google_docstring = False
